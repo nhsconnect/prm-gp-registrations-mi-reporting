1d8d8bdcb9f958c3e8b69d25a5d3aa3f8d921873
Caius Eugene <caius.eugene1@nhs.net>
2023-03-13 13:48:36 +0000

    Clean up

--- tests/test_base_reporting.py
+++ tests/test_base_reporting.py
@@ -7,35 +7,59 @@ from splunklib.binding import HTTPError
 from splunklib import client
 import splunklib.results as results
 import jq
+import re
 
 splunk_token = os.environ['SPLUNK_TOKEN']
 splunk_host = os.environ.get('SPLUNK_HOST')
 
 service = client.connect(token=splunk_token)
 
+
+def teardown_function():
+    service.indexes.delete("test_index")
+
+
 def test_reporting_window_then_return_event_within():
-    path = os.path.join(os.path.dirname(__file__), '../queries', 'gp2gp_reporting_window.splunk')
     index = get_or_create_index("test_index")
 
-    index.submit(json.dumps(create_sample_event(conversationId = "WITHIN_REPORT_WINDOW")), sourcetype="myevent")
-    index.submit(json.dumps(create_sample_event(conversationId = "OUTSIDE_REPORT_WINDOW")), sourcetype="myevent")
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversationId = "WITHIN_REPORT_WINDOW",
+                registrationEventDateTime = "2023-03-10T00:00:00"
+            )),
+            sourcetype="myevent")
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversationId = "OUTSIDE_REPORT_WINDOW",
+                registrationEventDateTime = "2023-03-20T00:00:00"
+            )),
+            sourcetype="myevent")
 
-    test_query = open(path).read()
-    test_query = set_variables_on_query(test_query, {
+    test_query = get_search('gp2gp_reporting_window')
+    test_query = set_variables_on_query(test_query, "test_index", {
         "$cutoff$": "2",
         "$report_start$": "2023-03-09",
         "$report_end$": "2023-03-11"
     })
+    print(test_query)
 
     telemetry = get_telemetry_from_splunk(test_query)
-
-    service.indexes.delete("test_index")
     
     assert len(telemetry) == 1
     assert jq.first('.[]._raw | fromjson.conversationId', telemetry) == 'WITHIN_REPORT_WINDOW'
 
 
-def create_sample_event(conversationId=str(uuid.uuid4())):
+def get_search(search_name):
+    path = os.path.join(os.path.dirname(__file__), '../queries', f'{search_name}.splunk')
+    return open(path).read()
+
+
+def create_sample_event(
+        conversationId=str(uuid.uuid4()),
+        registrationEventDateTime="2023-03-10T12:53:01"
+    ):
     return {
         "eventId":str(uuid.uuid4()),
         "eventGeneratedDateTime":"2023-03-20T12:53:01",
@@ -51,7 +75,7 @@ def create_sample_event(conversationId=str(uuid.uuid4())):
         "sendingPracticeIcbOdsCode":None,
         "sendingPracticeIcbName":None,
         "conversationId":conversationId,
-        "registrationEventDateTime":"2023-03-10T12:53:01",
+        "registrationEventDateTime":registrationEventDateTime,
         "payload":None
     }
 
@@ -63,8 +87,8 @@ def get_or_create_index(index_id):
     return service.indexes.create(index_id)
 
 
-def set_variables_on_query(search_query, vars):
-    result = search_query
+def set_variables_on_query(search_query, index, vars):
+    result = re.sub(r'index=[^|\s]+', f'index={index}', search_query)
     for key in vars:
         result = result.replace(key, vars[key])
     return result
@@ -78,8 +102,8 @@ def get_telemetry_from_splunk(search_query):
         return
     
     job = service.jobs.create(search_query,
-        earliest_time="-6w@w",
-        latest_time="@w",
+        earliest_time="-3y@d",
+        latest_time="+3y@d",
     )
     while True:
         while not job.is_ready():
