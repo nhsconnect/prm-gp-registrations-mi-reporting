7c34b8bab080e7a8b6da64e743d8815d6827ee0c
Caius Eugene <caius.eugene1@nhs.net>
2023-03-13 13:48:36 +0000

    Refactored helpers to seperate module

--- tests/test_base_reporting.py
+++ tests/test_base_reporting.py
@@ -1,13 +1,9 @@
 import os
 import json
 import os
-from time import sleep
-import uuid
-from splunklib.binding import HTTPError
 from splunklib import client
-import splunklib.results as results
 import jq
-import re
+from helpers.splunk import get_telemetry_from_splunk, get_or_create_index, create_sample_event, set_variables_on_query
 
 splunk_token = os.environ['SPLUNK_TOKEN']
 splunk_host = os.environ.get('SPLUNK_HOST')
@@ -20,7 +16,7 @@ def teardown_function():
 
 
 def test_reporting_window_then_return_event_within():
-    index = get_or_create_index("test_index")
+    index = get_or_create_index("test_index", service)
 
     index.submit(
         json.dumps(
@@ -45,7 +41,7 @@ def test_reporting_window_then_return_event_within():
     })
     print(test_query)
 
-    telemetry = get_telemetry_from_splunk(test_query)
+    telemetry = get_telemetry_from_splunk(test_query, service)
     
     assert len(telemetry) == 1
     assert jq.first('.[]._raw | fromjson.conversationId', telemetry) == 'WITHIN_REPORT_WINDOW'
@@ -54,74 +50,3 @@ def test_reporting_window_then_return_event_within():
 def get_search(search_name):
     path = os.path.join(os.path.dirname(__file__), '../queries', f'{search_name}.splunk')
     return open(path).read()
-
-
-def create_sample_event(
-        conversationId=str(uuid.uuid4()),
-        registrationEventDateTime="2023-03-10T12:53:01"
-    ):
-    return {
-        "eventId":str(uuid.uuid4()),
-        "eventGeneratedDateTime":"2023-03-20T12:53:01",
-        "eventType":"READY_TO_INTEGRATE_STATUSES",
-        "reportingSystemSupplier":"200000000260",
-        "reportingPracticeOdsCode":"A00029",
-        "requestingPracticeOdsCode":"A00029",
-        "requestingPracticeName":"GP A",
-        "requestingPracticeIcbOdsCode":None,
-        "requestingPracticeIcbName":None,
-        "sendingPracticeOdsCode":"B00157",
-        "sendingPracticeName":"GP B",
-        "sendingPracticeIcbOdsCode":None,
-        "sendingPracticeIcbName":None,
-        "conversationId":conversationId,
-        "registrationEventDateTime":registrationEventDateTime,
-        "payload":None
-    }
-
-
-def get_or_create_index(index_id):
-    if index_id in service.indexes:
-        return service.indexes[index_id]
-   
-    return service.indexes.create(index_id)
-
-
-def set_variables_on_query(search_query, index, vars):
-    result = re.sub(r'index=[^|\s]+', f'index={index}', search_query)
-    for key in vars:
-        result = result.replace(key, vars[key])
-    return result
-
-
-def get_telemetry_from_splunk(search_query):
-    try:
-        service.parse(search_query, parse_only=True)
-    except HTTPError as e:
-        print(f"query '{search_query}' is invalid:\n\t{str(e)}", 2)
-        return
-    
-    job = service.jobs.create(search_query,
-        earliest_time="-3y@d",
-        latest_time="+3y@d",
-    )
-    while True:
-        while not job.is_ready():
-            pass
-        stats = {'isDone': job['isDone'],
-                 'doneProgress': job['doneProgress'],
-                 'scanCount': job['scanCount'],
-                 'eventCount': job['eventCount'],
-                 'resultCount': job['resultCount']}
-        if stats['isDone'] == '1':
-            break
-        sleep(2)
-    rr = results.JSONResultsReader(job.results(output_mode='json'))
-    telemetry = []
-    for result in rr:
-        if isinstance(result, results.Message):
-            print(result.type, result.message)
-        elif isinstance(result, dict):
-            telemetry.append(result)
-    job.cancel()
-    return telemetry
