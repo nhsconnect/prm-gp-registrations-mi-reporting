364414337a44f24d27135ecfc13f729aa2428838
LFecci <lorenzo.fecci1@nhs.net>
2023-03-14 10:27:29 +0000

    Additional Business Process Report tests

--- tests/test_base_reporting.py
+++ tests/test_base_reporting.py
@@ -4,7 +4,7 @@ from time import sleep
 from splunklib import client
 import jq
 from helpers.splunk \
-import get_telemetry_from_splunk, get_or_create_index, create_sample_event, set_variables_on_query
+    import get_telemetry_from_splunk, get_or_create_index, create_sample_event, set_variables_on_query
 
 
 splunk_token = os.environ['SPLUNK_TOKEN']
@@ -18,7 +18,7 @@ def teardown_function():
     service.indexes.delete("test_index")
 
 
-def test_reporting_window_then_return_event_within() -> None :
+def test_reporting_window_then_return_event_within() -> None:
     """Function create test index."""
     index = get_or_create_index("test_index", service)
 
@@ -162,13 +162,145 @@ def test_business_process_report_integrated_within_8_days():
                     telemetry) == '1'
 
 
-# def test_business_process_report_not_integrated_within_8_days():
-#     pass
+def test_business_process_report_not_integrated_within_8_days():
+
+    # Arrange
+
+    index = get_or_create_index("test_index", service)
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id="NOT_INTERGRATED_LESS_THAN_8_DAYS",
+                registration_event_datetime="2023-03-10T08:00:00",
+                event_type="REGISTRATIONS"
+            )),
+        sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id="NOT_INTERGRATED_LESS_THAN_8_DAYS",
+                registration_event_datetime="2023-03-10T08:19:00",
+                event_type="READY_TO_INTEGRATE_STATUSES"
+            )),
+        sourcetype="myevent")
+
+    # Act
+
+    test_query = get_search('gp2gp_business_process_report')
+    test_query = set_variables_on_query(test_query, {
+        "$index$": "test_index",
+        "$cutoff$": "7",
+        "$report_start$": "2023-03-09",
+        "$report_end$": "2023-03-20"
+    })
+
+    sleep(2)
+
+    telemetry = get_telemetry_from_splunk(savedsearch(test_query), service)
+
+    # Assert
+
+    assert len(telemetry) == 4
+    assert jq.first('.[] | select( .label == "NOT_INTERGRATED_LESS_THAN_8_DAYS") | .count',
+                    telemetry) == '1'
 
 
-# def test_business_process_report_integrated_over_8_days():
-#     pass
+def test_business_process_report_integrated_over_8_days():
+
+    # Arrange
+
+    index = get_or_create_index("test_index", service)
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id="INTERGRATED_MORE_THAN_8_DAYS",
+                registration_event_datetime="2023-03-10T08:00:00",
+                event_type="REGISTRATIONS"
+            )),
+        sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id="INTERGRATED_MORE_THAN_8_DAYS",
+                registration_event_datetime="2023-03-10T08:19:00",
+                event_type="READY_TO_INTEGRATE_STATUSES"
+            )),
+        sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id="INTERGRATED_MORE_THAN_8_DAYS",
+                registration_event_datetime="2023-03-19T10:00:00",
+                event_type="EHR_INTEGRATIONS"
+            )),
+        sourcetype="myevent")
+
+    # Act
+
+    test_query = get_search('gp2gp_business_process_report')
+    test_query = set_variables_on_query(test_query, {
+        "$index$": "test_index",
+        "$cutoff$": "15",
+        "$report_start$": "2023-03-09",
+        "$report_end$": "2023-03-29"
+    })
+
+    sleep(2)
+
+    telemetry = get_telemetry_from_splunk(savedsearch(test_query), service)
+
+    # Assert
+
+    assert len(telemetry) == 4
+    assert jq.first('.[] | select( .label == "INTERGRATED_MORE_THAN_8_DAYS") | .count',
+                    telemetry) == '1'
 
 
-# def test_business_process_report_not_integrated_over_8_days():
-#     pass
+def test_business_process_report_not_integrated_over_8_days():
+
+    # Arrange
+
+    index = get_or_create_index("test_index", service)
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id="NOT_INTERGRATED_MORE_THAN_8_DAYS",
+                registration_event_datetime="2023-03-10T08:00:00",
+                event_type="REGISTRATIONS"
+            )),
+        sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id="NOT_INTERGRATED_MORE_THAN_8_DAYS",
+                registration_event_datetime="2023-03-10T08:19:00",
+                event_type="READY_TO_INTEGRATE_STATUSES"
+            )),
+        sourcetype="myevent")
+
+    # Act
+
+    test_query = get_search('gp2gp_business_process_report')
+    test_query = set_variables_on_query(test_query, {
+        "$index$": "test_index",
+        "$cutoff$": "15",
+        "$report_start$": "2023-03-09",
+        "$report_end$": "2023-03-29"
+    })
+
+    sleep(2)
+
+    telemetry = get_telemetry_from_splunk(savedsearch(test_query), service)
+
+    # Assert    
+
+    assert len(telemetry) == 4
+    assert jq.first('.[] | select( .label == "NOT_INTERGRATED_MORE_THAN_8_DAYS") | .count',
+                    telemetry) == '1'
