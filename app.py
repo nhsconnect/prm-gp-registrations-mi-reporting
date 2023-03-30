b575ed6b36cfd2df3c65fcc904fc5b244c6b1dfd
Caius Eugene <caius.eugene1@nhs.net>
2023-03-30 15:14:36 +0100

    Finished deploy reports function:

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import logging
 from chalice import Chalice
 
 from chalicelib.deploy_dashboards import deploy_dashboards
-from chalicelib.deploy_reports import deploy_saved_searches
+from chalicelib.deploy_reports import deploy_reports
 
 app = Chalice(app_name='mi-dashboard-deployer')
 logger = logging.getLogger("Dashboard-logging")
@@ -12,7 +12,7 @@ logger.setLevel(logging.DEBUG)
 @app.lambda_function(name='splunk-uploader')
 def main(event, context):
     logger.info("deploying saved searches...")
-    deploy_saved_searches()
+    deploy_reports()
     logger.info("deploying dashboards...")
     deploy_dashboards()
 
--- chalicelib/deploy_reports.py
+++ chalicelib/deploy_reports.py
@@ -2,31 +2,59 @@ import os
 from splunklib import client
 from os import listdir
 from os.path import isfile, join
+import boto3
+from botocore.exceptions import ClientError
 
-
+# set by pipeline
+splunk_host = os.environ.get('SPLUNK_HOST') or 'https://localhost:8089'
+splunk_admin_username = os.environ.get('SPLUNK_ADMIN_USERNAME')
+splunk_app_id = os.environ.get('SPLUNK_APP_ID') or 'search'
 splunk_token = os.environ['SPLUNK_TOKEN']
-splunk_host = os.environ.get('SPLUNK_HOST')
 
-def deploy_saved_searches():
+# s3 bucket
+bucket_name = os.environ['BUCKET_NAME']
 
+def check_env_variable(env_var: str) -> None:
+    try:
+        os.environ[env_var]
+    except KeyError:
+        print(f'Please set the environment variable: {env_var}')
+
+
+
+# check essential env variables
+check_env_variable("SPLUNK_HOST") # e.g. https://localhost:8089
+check_env_variable("SPLUNK_ADMIN_USERNAME") # user with admin role
+check_env_variable("SPLUNK_TOKEN") # token created with splunk
+
+client = boto3.client("s3")
+s3 = boto3.resource('s3')
+
+
+def get_or_create_bucket(name:str):
+
+    try:       
+        client.head_bucket(Bucket=name)
+    except ClientError:
+        # The bucket does not exist or you have no access.
+        client.create_bucket(Bucket=name, CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
+
+    return s3.Bucket(name)
+
+
+def deploy_reports():
+
+    # loop through dashboard files  
+    bucket = get_or_create_bucket(bucket_name)  #s3.Bucket("[Bucket Name]")   
     service = client.connect(token=splunk_token)
+    
+    for obj in bucket.objects.filter(Prefix='dashboards/'):        
 
-    path = os.path.join(os.path.dirname(__file__),
-                        '../queries')
+        # get the dashboard xml as a string
+        report_content = obj.get()['Body'].read().decode('utf-8')  
+        report_name = obj.key
 
-    query_filenames = [f for f in listdir(path) if isfile(join(path, f))]
+        if report_name in service.saved_searches:
+            service.saved_searches.delete(report_name)
 
-    for query_filename in query_filenames:
-
-        query_path = os.path.join(os.path.dirname(__file__),
-                        '../queries',query_filename)
-        
-        queryString = open(query_path, encoding="utf-8").read()
-
-        fileName = os.path.basename(query_path)
-
-        if fileName in service.saved_searches:
-            service.saved_searches.delete(fileName)
-
-        service.saved_searches.create(
-            fileName, queryString)
+        service.saved_searches.create(report_name, report_content)
