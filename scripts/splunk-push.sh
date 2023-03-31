d8f1fb0896e08997e27a4ced4be4efe15d736643
LFecci <lorenzo.fecci@madetech.com>
2023-03-31 09:54:48 +0100

    Removed unused code

--- scripts/splunk-push.sh
+++ scripts/splunk-push.sh
@@ -2,10 +2,6 @@
 
 set -o pipefail
 
-# via chalice (options) -
-# 1. chalice invoke -n main?
-# 2. curl s3://prm-gp-registrations-mi-reporting-dev/cdb017953dacb99de2059bc769124d2a - wrong.
-
 # aws cli -
 aws lambda invoke \
   --cli-binary-format raw-in-base64-out \
