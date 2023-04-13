b902871679db00872866caee40b1d2eefeb200fa
LFecci <lorenzo.fecci@madetech.com>
2023-04-13 11:25:20 +0100

    Added test_metrics_by_reg_status

--- tests/test_technical_failure_scenario.py
+++ tests/test_technical_failure_scenario.py
@@ -46,7 +46,7 @@ def teardown_function():
     """Function delete test_index."""
     service.indexes.delete("test_index")
 
-
+@pytest.mark.skip(reason="Being used as an example for other tests here.")
 def test_technical_failure_scenario_table():
     '''Tests the output for the technical failure scenario table'''
 
@@ -306,7 +306,75 @@ def test_technical_failure_scenario_table():
 
 
 def test_metrics_by_reg_status():
-    pass
+
+    # Arrange
+
+    index = get_or_create_index("test_index", service)
+
+    conversation_id = 'REG_STATUS_INTEGRATED'
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id,
+                registration_event_datetime="2023-03-10T08:00:00",
+                event_type=EventType.REGISTRATIONS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP"
+            )),
+        sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id,
+                registration_event_datetime="2023-03-10T08:19:00",
+                event_type=EventType.EHR_REQUEST.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP"
+            )),
+        sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id,
+                registration_event_datetime="2023-03-10T08:19:00",
+                event_type=EventType.EHR_RESPONSE.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP"
+            )),
+        sourcetype="myevent")
+   
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id,
+                registration_event_datetime="2023-03-10T08:19:00",
+                event_type=EventType.EHR_INTEGRATIONS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP"               
+            )),
+        sourcetype="myevent")
+    # Act
+
+    test_query = get_search('gp2gp_technical_failure_scenario_report')
+    test_query = set_variables_on_query(test_query, {
+        "$index$": "test_index",
+        "$report_start$": "2023-03-01",
+        "$report_end$": "2023-03-31"
+    })
+
+    sleep(2)
+
+    telemetry = get_telemetry_from_splunk(savedsearch(test_query), service)
+    LOG.info(f'telemetry: {telemetry}')
+
+    # Assert - check that there is 1 event each (count), 3 events in total (totalCount) and the percentage is 33.3
+    assert jq.first(
+        '.[] | select( .registrationStatus == "INTEGRATED" ) | select( .count == "1" )', telemetry)
+
 
 def test_metrics_by_sending_supplier():
     pass
