97a8df65d5057c82d6ca32e83f9656f4ba6426b9
LFecci <lorenzo.fecci@madetech.com>
2023-05-05 17:07:26 +0100

    work on sla status outcomes

--- tests/test_sla_status.py
+++ tests/test_sla_status.py
@@ -213,5 +213,332 @@ def test_total_transfer_time_outside_sla_24_hours():
         '| select( .totalTransferTimeOutsideSla24Hours=="3" )'           
         , telemetry)   
 
+def test_ehr_sending_outside_sla():
+
+     # Arrange
+
+    index = get_or_create_index("test_index", service)    
+
+     # test requires a datetime less than 20mins
+    now_minus_20_mins = datetime.today() - timedelta(hours=0, minutes=15)
+    LOG.info(f"now_minus_20_mins: {now_minus_20_mins}")
+
+    # test - #1 - outside sla
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_1',
+                registration_event_datetime="2023-03-10T09:00:00",
+                event_type= EventType.EHR_REQUESTS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    # test - #2 - inside sla  
 
 
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_2',
+                registration_event_datetime=now_minus_20_mins.strftime("%Y-%m-%dT%H:%M:%S"),
+                event_type= EventType.EHR_REQUESTS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    # test - #3 - outside sla READY_TO_INTEGRATE
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_ready_to_integrate',
+                registration_event_datetime="2023-03-10T09:00:00",
+                event_type= EventType.EHR_REQUESTS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_ready_to_integrate',
+                registration_event_datetime="2023-03-10T10:00:00",
+                event_type= EventType.EHR_RESPONSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_ready_to_integrate',
+                registration_event_datetime="2023-03-10T11:00:00",
+                event_type= EventType.READY_TO_INTEGRATE_STATUSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+     # test - #4 - inside sla READY_TO_INTEGRATE
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_inside_sla_ready_to_integrate',
+                registration_event_datetime="2023-03-10T09:00:00",
+                event_type= EventType.EHR_REQUESTS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_inside_sla_ready_to_integrate',
+                registration_event_datetime="2023-03-10T09:05:00",
+                event_type= EventType.EHR_RESPONSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_inside_sla_ready_to_integrate',
+                registration_event_datetime="2023-03-10T10:00:00",
+                event_type= EventType.READY_TO_INTEGRATE_STATUSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+     # test - #5 - inside sla INTEGRATED
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_inside_sla_integrated',
+                registration_event_datetime="2023-03-10T09:00:00",
+                event_type= EventType.EHR_REQUESTS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_inside_sla_integrated',
+                registration_event_datetime="2023-03-10T09:05:00",
+                event_type= EventType.EHR_RESPONSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_inside_sla_integrated',
+                registration_event_datetime="2023-03-10T10:01:00",
+                event_type= EventType.READY_TO_INTEGRATE_STATUSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_inside_sla_integrated',
+                registration_event_datetime="2023-03-10T10:30:00",
+                event_type=EventType.EHR_INTEGRATIONS.value,
+                payload = create_integration_payload(outcome="INTEGRATED")                
+            )),
+        sourcetype="myevent")
+    
+     # test - #6 - outside sla INTEGRATED
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_integrated',
+                registration_event_datetime="2023-03-10T09:00:00",
+                event_type= EventType.EHR_REQUESTS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_integrated',
+                registration_event_datetime="2023-03-10T10:00:00",
+                event_type= EventType.EHR_RESPONSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_integrated',
+                registration_event_datetime="2023-03-10T10:01:00",
+                event_type= EventType.READY_TO_INTEGRATE_STATUSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_integrated',
+                registration_event_datetime="2023-03-10T10:30:00",
+                event_type=EventType.EHR_INTEGRATIONS.value,
+                payload = create_integration_payload(outcome="INTEGRATED")                
+            )),
+        sourcetype="myevent")
+    
+    # test #7 - EHR_SENT - outside sla
+    
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_ehr_sent',
+                registration_event_datetime="2023-03-10T09:00:00",
+                event_type= EventType.EHR_REQUESTS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")    
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_outside_sla_ehr_sent',
+                registration_event_datetime="2023-03-10T10:00:00",
+                event_type= EventType.EHR_RESPONSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+    # test #8 - EHR_SENT - inside sla
+    
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_inside_sla_ehr_sent',
+                registration_event_datetime="2023-03-10T09:00:00",
+                event_type= EventType.EHR_REQUESTS.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")    
+
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                conversation_id='test_ehr_sending_inside_sla_ehr_sent',
+                registration_event_datetime="2023-03-10T09:15:00",
+                event_type= EventType.EHR_RESPONSES.value,
+                sendingPracticeSupplierName="EMIS",
+                requestingPracticeSupplierName="TPP" 
+            )),
+    sourcetype="myevent")
+
+
+
+     # Act
+
+    test_query = get_search('gp2gp_sla_outcomes')
+    test_query = set_variables_on_query(test_query, {
+        "$index$": "test_index",
+        "$report_start$": "2023-03-01",
+        "$report_end$": "2023-03-31"
+    })
+
+    sleep(2)
+
+    telemetry = get_telemetry_from_splunk(savedsearch(test_query), service)
+    LOG.info(f'telemetry: {telemetry}')
+
+    # Assert
+    assert jq.first(
+        '.[] '+
+        '| select( .total_ehr_sending_outside_sla=="4" )'           
+        , telemetry)   
+    
+
+def test_ehr_requesting_outside_sla():
+
+     # Arrange
+
+    index = get_or_create_index("test_index", service)    
+
+    # test #1 - Eligibile for transfer outside SLA
+    
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                "test_ehr_requesting_outside_sla_test#1",
+                registration_event_datetime="2023-03-10T09:15:00",
+                event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                payload=create_transfer_compatibility_payload(
+                    internalTransfer=False,
+                    transferCompatible=True
+                )
+            )),
+        sourcetype="myevent")
+    
+    # test #2 - Eligibile for transfer inside SLA
+
+      # test requires a datetime less than 20mins
+    now_minus_20_mins = datetime.today() - timedelta(hours=0, minutes=15)
+    LOG.info(f"now_minus_20_mins: {now_minus_20_mins}")
+    
+    index.submit(
+        json.dumps(
+            create_sample_event(
+                "test_ehr_requesting_inside_sla_test#2",
+                registration_event_datetime=now_minus_20_mins.strftime("%Y-%m-%dT%H:%M:%S"),
+                event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                payload=create_transfer_compatibility_payload(
+                    internalTransfer=False,
+                    transferCompatible=True
+                )
+            )),
+        sourcetype="myevent")
+
+
+
+    # Act
+
+    test_query = get_search('gp2gp_sla_outcomes')
+    test_query = set_variables_on_query(test_query, {
+        "$index$": "test_index",
+        "$report_start$": "2023-03-01",
+        "$report_end$": "2023-03-31"
+    })
+
+    sleep(2)
+
+    telemetry = get_telemetry_from_splunk(savedsearch(test_query), service)
+    LOG.info(f'telemetry: {telemetry}')
+
+    # Assert
+    assert jq.first(
+        '.[] '+
+        '| select( .total_ehr_requesting_outside_sla=="1" )'           
+        , telemetry)   
