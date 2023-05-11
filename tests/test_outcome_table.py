576bc729d6cfb5baab2d6f6ade894097efd63524
LFecci <lorenzo.fecci@madetech.com>
2023-05-11 14:58:34 +0100

    test technical outcome test_not_eligible_for_electronic_transfer
    Co-authored-by: JoeSykesNHS <JoeSykesNHS@users.noreply.github.com>

--- tests/test_outcome_table.py
+++ tests/test_outcome_table.py
@@ -8,7 +8,7 @@ from splunklib import client
 import jq
 from helpers.splunk \
     import get_telemetry_from_splunk, get_or_create_index, create_sample_event, set_variables_on_query, \
-    create_integration_payload,  create_error_payload, create_transfer_compatibility_payload
+    create_integration_payload,  create_error_payload, create_transfer_compatibility_payload, create_registration_payload
 from datetime import datetime, timedelta, date
 from jinja2 import Environment, FileSystemLoader
 from .base_test_report import splunk_index
@@ -684,6 +684,79 @@ def test_outcome_in_progress_3():
         # Assert -
         assert jq.first(
             '.[] | select( .outcome == "IN_PROGRESS" ) | select( .count == "1" )', telemetry)
+
+    finally:
+        splunk_index.delete(index_name)
+
+
+def test_not_eligible_for_electronic_transfer():
+    '''
+    This test requires an EHR_REQUEST within 20 mins to get an EHR_SENDING_OUTSIDE_SLA = false (test 1.a).
+    Test (1.b) is there to validate the test 1.a is the only one with EHR_SENDING_OUTSIDE_SLA = true.
+    Registraion status for this test should be EHR_REQUESTED.    
+    '''
+
+    # Arrange
+
+    index_name, index = splunk_index.create(service)
+
+    # reporting window
+    report_start = datetime.today().date().replace(day=1)
+    report_end = datetime.today().date().replace(day=31)
+
+    try:
+
+        # test 1.a - NOT ELIGIBLE FOR ELECTRONIC TRANSFER - transfer_compatible = false       
+
+        conversation_id = 'test_not_eligible_for_electronic_transfer_compatible_false'
+
+        index.submit(
+            json.dumps(
+                create_sample_event(
+                    conversation_id=conversation_id,
+                    registration_event_datetime = create_date_time(datetime.today(),"07:00:00"),
+                    event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                    payload=create_transfer_compatibility_payload(
+                        internalTransfer=False,
+                        transferCompatible=False
+                    )
+                )),
+            sourcetype="myevent")             
+
+      
+
+        # test 1.b - NOT ELIGIBLE FOR ELECTRONIC TRANSFER - status = REGISTRATION and D.T.S matched = false
+
+        conversation_id = 'test_not_eligible_for_electronic_transfer_dts_trace_false'      
+
+        index.submit(
+            json.dumps(
+                create_sample_event(
+                    conversation_id=conversation_id,
+                    registration_event_datetime = create_date_time(datetime.today(),"04:30:00"),
+                    event_type=EventType.REGISTRATIONS.value,
+                    payload=create_registration_payload(dtsMatched=False)
+                )),
+            sourcetype="myevent")  
+      
+
+        # Act
+
+        test_query = get_search('gp2gp_outcome_report')
+        test_query = set_variables_on_query(test_query, {
+            "$index$": index_name,
+            "$report_start$": report_start.strftime("%Y-%m-%d"),
+            "$report_end$": report_end.strftime("%Y-%m-%d")
+        })
+
+        sleep(2)
+
+        telemetry = get_telemetry_from_splunk(savedsearch(test_query), service)
+        LOG.info(f'telemetry: {telemetry}')
+
+        # Assert -
+        assert jq.first(
+            '.[] | select( .outcome == "NOT_ELIGIBLE_FOR_ELECTRONIC_TRANSFER" ) | select( .count == "2" )', telemetry)
 
     finally:
         splunk_index.delete(index_name)
