09778641e9975e3bc08217763e408b3d59835174
LFecci <lorenzo.fecci@madetech.com>
2023-05-11 12:23:51 +0100

    test technical outcome IN_PROGRESS_3

--- tests/test_outcome_table.py
+++ tests/test_outcome_table.py
@@ -607,3 +607,83 @@ def test_technical_failure_4():
 
     finally:
         splunk_index.delete(index_name)
+
+
+def test_outcome_in_progress_3():
+    '''
+    This test requires an EHR_REQUEST within 20 mins to get an EHR_SENDING_OUTSIDE_SLA = false (test 1.a).
+    Test (1.b) is there to validate the test 1.a is the only one with EHR_SENDING_OUTSIDE_SLA = true.
+    Registraion status for this test should be EHR_REQUESTED.    
+    '''
+
+    # Arrange
+
+    index_name, index = splunk_index.create(service)
+
+    # reporting window
+    report_start = datetime.today().date().replace(day=1)
+    report_end = datetime.today().date().replace(day=31)
+
+    try:
+
+        # test 1.a - inside SLA
+
+        # test requires a datetime less than 20mins
+        now_minus_18_mins = datetime.today() - timedelta(hours=0, minutes=18)
+        LOG.info(f"now_minus_18_mins: {now_minus_18_mins}")     
+
+        conversation_id = 'test_outcome_in_progress_3_inside_sla'
+
+        index.submit(
+            json.dumps(
+                create_sample_event(
+                    conversation_id=conversation_id,
+                    registration_event_datetime= now_minus_18_mins.strftime("%Y-%m-%dT%H:%M:%S"),
+                    event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                    payload=create_transfer_compatibility_payload(
+                        internalTransfer=False,
+                        transferCompatible=True
+                    )
+                )),
+            sourcetype="myevent")             
+
+      
+
+        # test 1.b - outside SLA
+
+        conversation_id = 'test_outcome_in_progress_3_outside_sla'      
+
+        index.submit(
+            json.dumps(
+                create_sample_event(
+                    conversation_id=conversation_id,
+                    registration_event_datetime = create_date_time(datetime.today(),"04:30:00"),
+                    event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                    payload=create_transfer_compatibility_payload(
+                        internalTransfer=False,
+                        transferCompatible=True
+                    )
+                )),
+            sourcetype="myevent")  
+      
+
+        # Act
+
+        test_query = get_search('gp2gp_outcome_report')
+        test_query = set_variables_on_query(test_query, {
+            "$index$": index_name,
+            "$report_start$": report_start.strftime("%Y-%m-%d"),
+            "$report_end$": report_end.strftime("%Y-%m-%d")
+        })
+
+        sleep(2)
+
+        telemetry = get_telemetry_from_splunk(savedsearch(test_query), service)
+        LOG.info(f'telemetry: {telemetry}')
+
+        # Assert -
+        assert jq.first(
+            '.[] | select( .outcome == "IN_PROGRESS" ) | select( .count == "1" )', telemetry)
+
+    finally:
+        splunk_index.delete(index_name)
