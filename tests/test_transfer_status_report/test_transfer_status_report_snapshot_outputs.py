cd87b55871846895b23a820b0561d3eac847cc7a
LFecci <lorenzo.fecci@madetech.com>
2023-05-31 16:56:30 +0100

    Added query for total eligible for transfer for use in dashboard

--- tests/test_transfer_status_report/test_transfer_status_report_snapshot_outputs.py
+++ tests/test_transfer_status_report/test_transfer_status_report_snapshot_outputs.py
@@ -178,14 +178,97 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
             self.LOG.info(f'telemetry: {telemetry}')
 
             # Assert
-            assert jq.first(
-                '.[] ' +
-                '| select( .total_eligible_for_electronic_transfer=="2" )' +
-                '| select( .percentage_successfully_integrated == "0.00")' +
-                '| select( .percentage_rejected == "0.00")' +
-                '| select( .percentage_awaiting_integration == "0.00")' +
-                '| select( .percentage_in_progress == "0.00")' +
-                '| select( .percentage_technical_failure == "100.00")', telemetry)
+            expected_values = {"Successfully Integrated": "0.00",
+                               "Rejected": "0.00",
+                               "Awaiting Integration": "0.00", 
+                               "In-Progress": "0.00", 
+                               "Technical Failure": "100.00"}
+
+            for idx, (key, value) in enumerate(expected_values.items()):
+                assert jq.first(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")', telemetry)
 
         finally:
             self.delete_index(index_name)
+
+    def test_gp2gp_transfer_status_report_snapshot_total_eligible_for_transfer(self):
+
+            # Arrange
+            index_name, index = self.create_index()
+
+            try:
+
+                index.submit(
+                    json.dumps(
+                        create_sample_event(
+                            'test_total_eligible_for_electronic_transfer_1',
+                            registration_event_datetime="2023-03-10T08:00:00",
+                            event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                            sendingPracticeSupplierName="EMIS",
+                            requestingPracticeSupplierName="TPP",
+                            payload=create_transfer_compatibility_payload(
+                                internalTransfer=False,
+                                transferCompatible=True,
+                                reason="test1"
+                            )
+
+                        )),
+                    sourcetype="myevent")
+
+                index.submit(
+                    json.dumps(
+                        create_sample_event(
+                            'test_total_eligible_for_electronic_transfer_2',
+                            registration_event_datetime="2023-03-10T09:00:00",
+                            event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                            sendingPracticeSupplierName="EMIS",
+                            requestingPracticeSupplierName="TPP",
+                            payload=create_transfer_compatibility_payload(
+                                internalTransfer=False,
+                                transferCompatible=True,
+                                reason="test2"
+                            )
+
+                        )),
+                    sourcetype="myevent")
+
+                index.submit(
+                    json.dumps(
+                        create_sample_event(
+                            'test_total_eligible_for_electronic_transfer_3',
+                            registration_event_datetime="2023-03-10T10:00:00",
+                            event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                            sendingPracticeSupplierName="EMIS",
+                            requestingPracticeSupplierName="TPP",
+                            payload=create_transfer_compatibility_payload(
+                                internalTransfer=True,
+                                transferCompatible=True,
+                                reason="test1"
+                            )
+
+                        )),
+                    sourcetype="myevent")
+
+                # Act
+                test_query = self.get_search(
+                    'gp2gp_transfer_status_report/gp2gp_transfer_status_report_snapshot_total_eligible_for_transfer')
+
+                test_query = set_variables_on_query(test_query, {
+                    "$index$": index_name,
+                    "$report_start$": "2023-03-01",
+                    "$report_end$": "2023-03-31"
+                })
+
+                sleep(2)
+
+                telemetry = get_telemetry_from_splunk(
+                    self.savedsearch(test_query), self.splunk_service)
+                self.LOG.info(f'telemetry: {telemetry}')
+
+
+                # Assert                
+                assert jq.first(
+                    f'.[] | select( .total_eligible_for_electronic_transfer=="2")', telemetry)
+
+            finally:
+                self.delete_index(index_name)
