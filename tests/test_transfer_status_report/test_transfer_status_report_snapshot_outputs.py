80058939fe3dedb8fdcd3ef962817f79a437b8aa
LFecci <lorenzo.fecci@madetech.com>
2023-05-31 15:26:08 +0100

    Updated TSR report counts output
    Co-authored-by: JoeSykesNHS <JoeSykesNHS@users.noreply.github.com>

--- tests/test_transfer_status_report/test_transfer_status_report_snapshot_outputs.py
+++ tests/test_transfer_status_report/test_transfer_status_report_snapshot_outputs.py
@@ -88,104 +88,104 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 self.savedsearch(test_query), self.splunk_service)
             self.LOG.info(f'telemetry: {telemetry}')
 
-              # Assert
-            assert jq.first(
-                '.[] ' +
-                '| select( .total_eligible_for_electronic_transfer=="2" )' +
-                '| select( .count_successfully_integrated == "0")' +
-                '| select( .count_rejected == "0")' +
-                '| select( .count_awaiting_integration == "0")' +
-                '| select( .count_in_progress == "0")' +
-                '| select( .count_technical_failure == "2")', telemetry)
+            # Assert
+            expected_values = {"Successfully Integrated": "0",
+                               "Rejected": "0",
+                               "Awaiting Integration": "0", 
+                               "In-Progress": "0", 
+                               "Technical Failure": "2"}
+
+            for idx, (key, value) in enumerate(expected_values.items()):
+                assert jq.first(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")', telemetry)
 
         finally:
             self.delete_index(index_name)
 
     def test_gp2gp_transfer_status_report_snapshot_percentages(self):
-            '''This test just tests the "percentages output" is calling the base query correctly.'''
-            
-            # Arrange
-            index_name, index = self.create_index()      
+        '''This test just tests the "percentages output" is calling the base query correctly.'''
 
-            try:
+        # Arrange
+        index_name, index = self.create_index()
 
-                index.submit(
-                    json.dumps(
-                        create_sample_event(
-                            'test_total_eligible_for_electronic_transfer_1',
-                            registration_event_datetime="2023-03-10T08:00:00",
-                            event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
-                            sendingPracticeSupplierName="EMIS",
-                            requestingPracticeSupplierName="TPP",
-                            payload=create_transfer_compatibility_payload(
-                                internalTransfer=False,
-                                transferCompatible=True,
-                                reason="test1"
-                            )
+        try:
 
-                        )),
-                    sourcetype="myevent")
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        'test_total_eligible_for_electronic_transfer_1',
+                        registration_event_datetime="2023-03-10T08:00:00",
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingPracticeSupplierName="EMIS",
+                        requestingPracticeSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test1"
+                        )
 
-                index.submit(
-                    json.dumps(
-                        create_sample_event(
-                            'test_total_eligible_for_electronic_transfer_2',
-                            registration_event_datetime="2023-03-10T09:00:00",
-                            event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
-                            sendingPracticeSupplierName="EMIS",
-                            requestingPracticeSupplierName="TPP",
-                            payload=create_transfer_compatibility_payload(
-                                internalTransfer=False,
-                                transferCompatible=True,
-                                reason="test2"
-                            )
+                    )),
+                sourcetype="myevent")
 
-                        )),
-                    sourcetype="myevent")
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        'test_total_eligible_for_electronic_transfer_2',
+                        registration_event_datetime="2023-03-10T09:00:00",
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingPracticeSupplierName="EMIS",
+                        requestingPracticeSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test2"
+                        )
 
-                index.submit(
-                    json.dumps(
-                        create_sample_event(
-                            'test_total_eligible_for_electronic_transfer_3',
-                            registration_event_datetime="2023-03-10T10:00:00",
-                            event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
-                            sendingPracticeSupplierName="EMIS",
-                            requestingPracticeSupplierName="TPP",
-                            payload=create_transfer_compatibility_payload(
-                                internalTransfer=True,
-                                transferCompatible=True,
-                                reason="test1"
-                            )
+                    )),
+                sourcetype="myevent")
 
-                        )),
-                    sourcetype="myevent")
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        'test_total_eligible_for_electronic_transfer_3',
+                        registration_event_datetime="2023-03-10T10:00:00",
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingPracticeSupplierName="EMIS",
+                        requestingPracticeSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=True,
+                            transferCompatible=True,
+                            reason="test1"
+                        )
 
-                # Act
-                test_query = self.get_search('gp2gp_transfer_status_report/gp2gp_transfer_status_report_snapshot_percentages')
-                
-                test_query = set_variables_on_query(test_query, {
-                    "$index$": index_name,
-                    "$report_start$": "2023-03-01",
-                    "$report_end$": "2023-03-31"
-                })
+                    )),
+                sourcetype="myevent")
 
-                sleep(2)
+            # Act
+            test_query = self.get_search(
+                'gp2gp_transfer_status_report/gp2gp_transfer_status_report_snapshot_percentages')
 
-                telemetry = get_telemetry_from_splunk(
-                    self.savedsearch(test_query), self.splunk_service)
-                self.LOG.info(f'telemetry: {telemetry}')
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$report_start$": "2023-03-01",
+                "$report_end$": "2023-03-31"
+            })
 
-                # Assert
-                assert jq.first(
-                    '.[] ' +
-                    '| select( .total_eligible_for_electronic_transfer=="2" )' +
-                    '| select( .percentage_successfully_integrated == "0.00")' +
-                    '| select( .percentage_rejected == "0.00")' +
-                    '| select( .percentage_awaiting_integration == "0.00")' +
-                    '| select( .percentage_in_progress == "0.00")' +
-                    '| select( .percentage_technical_failure == "100.00")'
-                    , telemetry)
-            
+            sleep(2)
 
-            finally:
-                self.delete_index(index_name)
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            assert jq.first(
+                '.[] ' +
+                '| select( .total_eligible_for_electronic_transfer=="2" )' +
+                '| select( .percentage_successfully_integrated == "0.00")' +
+                '| select( .percentage_rejected == "0.00")' +
+                '| select( .percentage_awaiting_integration == "0.00")' +
+                '| select( .percentage_in_progress == "0.00")' +
+                '| select( .percentage_technical_failure == "100.00")', telemetry)
+
+        finally:
+            self.delete_index(index_name)
