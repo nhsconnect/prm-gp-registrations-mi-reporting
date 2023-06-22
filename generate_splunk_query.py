322da6a56ed4ad6c77d5cd4d07739e200fc30214
LFecci <lorenzo.fecci@madetech.com>
2023-06-22 15:50:28 +0100

    Created script to generate splunk query from report

---
+++ generate_splunk_query.py
@@ -0,0 +1,10 @@
+import argparse
+from helpers.splunk import generate_splunk_query_from_report
+
+# helper script to translate .splunk files in reports folder into runnable splunk queries
+ap = argparse.ArgumentParser()
+ap.add_argument("-r", "--report", required=True, help="path to the report")
+
+args = vars(ap.parse_args())
+
+generate_splunk_query_from_report(None,args['report'])
