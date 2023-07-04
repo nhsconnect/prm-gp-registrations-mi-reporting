7d287cd19df4217900559d3158f2d62737183c32
JoeSykesNHS <joe.sykes1@nhs.net>
2023-07-04 13:39:41 +0100

    fixed tests failing due to utc datetime

--- helpers/datetime_helper.py
+++ helpers/datetime_helper.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime, timezone, timedelta
 
 def create_date_time(date: date, time: str):
-    return date.strftime(f"%Y-%m-%dT{time}")
+    return date.strftime(f"%Y-%m-%dT{time}+00:00")
 
 def datetime_utc_now():
     return datetime.now(timezone.utc)
