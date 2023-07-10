d9dcf54d9ca2535d9e96d476f0c5a65647d3d34a
LFecci <lorenzo.fecci1@nhs.net>
2023-07-10 15:12:09 +0100

    Added test_missing_attachments_report_trending_percentages_with_start_end_times_as_datetimes

--- tests/test_missing_attachments_trending_report/test_missing_attachments_trending_outputs.py
+++ tests/test_missing_attachments_trending_report/test_missing_attachments_trending_outputs.py
@@ -77,6 +77,68 @@ class TestMissingAttachmentsTrendingOutputs(TestBase):
         finally:
             self.delete_index(index_name)
 
+    def test_missing_attachments_report_trending_percentages_with_start_end_times_as_datetimes(self):
+
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = datetime.today().date().replace(day=1)
+        report_end = datetime.today().date().replace(day=28)
+        cutoff = "0"
+
+        try:
+
+            conversationId = 'transfered_with_no_missing_attachments'
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=conversationId,
+                        registration_event_datetime=create_date_time(
+                            date=report_start, time="08:00:00"),
+                        event_type=EventType.EHR_RESPONSES.value,
+                        payload=create_ehr_response_payload(
+                            number_of_placeholders=0)
+                    )),
+                sourcetype="myevent")
+            
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=conversationId,
+                        registration_event_datetime=create_date_time(
+                            date=report_start, time="09:00:00"),
+                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value                      
+                    )),
+                sourcetype="myevent")
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_missing_attachments_trending_report/gp2gp_missing_attachments_trending_report_percentages')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$cutoff$": cutoff,
+                "$time_period$": "month"
+            })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            assert jq.first(
+                '.[] | select( ."Transfered with no missing attachments" == "100.00" ) ', telemetry)
+
+
+        finally:
+            self.delete_index(index_name)
+
     @pytest.mark.parametrize("cutoff, registrationStatus",[(1,"REGISTRATION"),(7,"EHR_REQUESTED"),(11,"EHR_SENT"),(19,"READY_TO_INTEGRATE")])
     def test_missing_attachments_report_cutoffs(self, cutoff, registrationStatus):
             """This test ensures that new conversations are at a different stage based on cutoff."""
