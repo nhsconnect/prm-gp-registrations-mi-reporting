b67d61e65bfa271789783748a057e4a6bcc01420
JoeSykesNHS <joe.sykes1@nhs.net>
2023-07-26 13:13:55 +0100

    [PRMT-3580] [JS] Created query to return the total number of records that are eligible for transfer.

--- tests/test_transfer_status_trending_report/test_transfer_status_report_trending_outputs.py
+++ tests/test_transfer_status_trending_report/test_transfer_status_report_trending_outputs.py
@@ -7,10 +7,10 @@ from time import sleep
 from splunklib import client
 import jq
 
-from helpers.datetime_helper import datetime_utc_now
+from helpers.datetime_helper import datetime_utc_now, create_date_time
 from helpers.splunk \
     import get_telemetry_from_splunk, get_or_create_index, create_sample_event, set_variables_on_query, \
-    create_integration_payload,  create_error_payload, create_transfer_compatibility_payload
+    create_integration_payload, create_error_payload, create_transfer_compatibility_payload
 from datetime import datetime, timedelta
 from jinja2 import Environment, FileSystemLoader
 from tests.test_base import TestBase, EventType
@@ -402,7 +402,6 @@ class TestTransferStatusReportTrendingOutputs(TestBase):
                     )),
                 sourcetype="myevent")
 
-
             index.submit(
                 json.dumps(
                     create_sample_event(
@@ -428,8 +427,6 @@ class TestTransferStatusReportTrendingOutputs(TestBase):
                         event_type=EventType.READY_TO_INTEGRATE_STATUSES.value
                     )),
                 sourcetype="myevent")
-
-
 
             # Act
             test_query = self.generate_splunk_query_from_report(
@@ -503,7 +500,6 @@ class TestTransferStatusReportTrendingOutputs(TestBase):
                     )),
                 sourcetype="myevent")
 
-
             index.submit(
                 json.dumps(
                     create_sample_event(
@@ -529,8 +525,6 @@ class TestTransferStatusReportTrendingOutputs(TestBase):
                         event_type=EventType.READY_TO_INTEGRATE_STATUSES.value
                     )),
                 sourcetype="myevent")
-
-
 
             # Act
             test_query = self.generate_splunk_query_from_report(
@@ -952,7 +946,6 @@ class TestTransferStatusReportTrendingOutputs(TestBase):
                     )),
                 sourcetype="myevent")
 
-
             index.submit(
                 json.dumps(
                     create_sample_event(
@@ -978,8 +971,6 @@ class TestTransferStatusReportTrendingOutputs(TestBase):
                         event_type=EventType.READY_TO_INTEGRATE_STATUSES.value
                     )),
                 sourcetype="myevent")
-
-
 
             # Act
             test_query = self.generate_splunk_query_from_report(
@@ -1053,7 +1044,6 @@ class TestTransferStatusReportTrendingOutputs(TestBase):
                     )),
                 sourcetype="myevent")
 
-
             index.submit(
                 json.dumps(
                     create_sample_event(
@@ -1079,8 +1069,6 @@ class TestTransferStatusReportTrendingOutputs(TestBase):
                         event_type=EventType.READY_TO_INTEGRATE_STATUSES.value
                     )),
                 sourcetype="myevent")
-
-
 
             # Act
             test_query = self.generate_splunk_query_from_report(
@@ -1118,4 +1106,91 @@ class TestTransferStatusReportTrendingOutputs(TestBase):
         finally:
             self.delete_index(index_name)
 
+    def test_gp2gp_transfer_status_report_trending_total_eligible_for_transfer(self):
 
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = datetime.today().date().replace(day=1)
+        report_end = datetime.today().date().replace(day=2)
+        cutoff = "0"
+
+        try:
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        'test_total_eligible_for_electronic_transfer_1',
+                        registration_event_datetime=create_date_time(report_start, "08:00:00"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingPracticeSupplierName="EMIS",
+                        requestingPracticeSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test1"
+                        )
+
+                    )),
+                sourcetype="myevent")
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        'test_total_eligible_for_electronic_transfer_2',
+                        registration_event_datetime=create_date_time(report_start, "09:00:00"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingPracticeSupplierName="EMIS",
+                        requestingPracticeSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=True,
+                            transferCompatible=True,
+                            reason="test2"
+                        )
+
+                    )),
+                sourcetype="myevent")
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        'test_total_eligible_for_electronic_transfer_3',
+                        registration_event_datetime=create_date_time(report_start, "10:00:00"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingPracticeSupplierName="EMIS",
+                        requestingPracticeSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test1"
+                        )
+
+                    )),
+                sourcetype="myevent")
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_transfer_status_trending_report/'
+                'gp2gp_transfer_status_trending_report_total_eligible_for_transfer')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$cutoff$": cutoff,
+                "$time_period$": "week"
+            })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            assert jq.first(
+                f'.[] | select( .total_eligible_for_electronic_transfer=="2")', telemetry)
+
+        finally:
+            self.delete_index(index_name)
