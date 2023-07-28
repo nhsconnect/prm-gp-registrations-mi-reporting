32ce9c4c01e4f84e489121fd5a495d0d09dcc51c
LFecci <lorenzo.fecci1@nhs.net>
2023-07-28 13:13:48 +0100

    Updated report start, end times

--- tests/test_integration_sla_status.py
+++ tests/test_integration_sla_status.py
@@ -9,7 +9,7 @@ import jq
 from helpers.splunk \
     import get_telemetry_from_splunk, get_or_create_index, create_sample_event, set_variables_on_query, \
     create_integration_payload,  create_error_payload, create_transfer_compatibility_payload
-from helpers.datetime_helper import datetime_utc_now
+from helpers.datetime_helper import datetime_utc_now, generate_report_start_date, generate_report_end_date
 from datetime import timedelta
 from jinja2 import Environment, FileSystemLoader
 from tests.test_base import TestBase, EventType
@@ -23,8 +23,8 @@ class TestIntegrationSlaStatus(TestBase):
         index_name, index = self.create_index()
 
         # reporting window
-        report_start = datetime_utc_now().date().replace(day=1)
-        report_end = datetime_utc_now().date().replace(day=28)
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
 
         try:       
 
@@ -106,8 +106,8 @@ class TestIntegrationSlaStatus(TestBase):
         index_name, index = self.create_index()
 
         # reporting window
-        report_start = datetime_utc_now().date().replace(day=1)
-        report_end = datetime_utc_now().date().replace(day=28)
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
 
         try:              
 
@@ -191,8 +191,8 @@ class TestIntegrationSlaStatus(TestBase):
         index_name, index = self.create_index()
 
         # reporting window
-        report_start = datetime_utc_now().date().replace(day=1)
-        report_end = datetime_utc_now().date().replace(day=28)
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
 
         try:              
 
@@ -277,8 +277,8 @@ class TestIntegrationSlaStatus(TestBase):
         index_name, index = self.create_index()
 
         # reporting window
-        report_start = datetime_utc_now().date().replace(day=1)
-        report_end = datetime_utc_now().date().replace(day=28)
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
 
         try:              
 
