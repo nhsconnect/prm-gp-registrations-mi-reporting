54ff10a44041eb704bac95afb832ede3840d179c
LFecci <lorenzo.fecci1@nhs.net>
2023-08-07 16:37:03 +0100

    placeholders breakdown graph - new queries and tests

---
+++ tests/test_placeholder_snapshot_report/test_placeholder_breakdown_graph.py
@@ -0,0 +1,154 @@
+import pytest
+import json
+from time import sleep
+import jq
+from helpers.splunk \
+    import get_telemetry_from_splunk, create_sample_event, set_variables_on_query, \
+    create_ehr_response_payload
+from tests.test_base import TestBase, EventType
+from helpers.datetime_helper import create_date_time, generate_report_start_date, generate_report_end_date
+import uuid
+
+class TestPlaceholderBreakdownGraph(TestBase):
+
+    @pytest.mark.parametrize("placeholders,output", [
+        (5, {"1_to_5_placeholders": 1, "6_to_10_placeholders": 0, "11_to_15_placeholders": 0, "16_to_20_placeholders": 0, "21_plus_placeholders": 0 }),
+        (6, {"1_to_5_placeholders": 0, "6_to_10_placeholders": 1, "11_to_15_placeholders": 0, "16_to_20_placeholders": 0, "21_plus_placeholders": 0 }),
+        (11, {"1_to_5_placeholders": 0, "6_to_10_placeholders": 0, "11_to_15_placeholders": 1, "16_to_20_placeholders": 0, "21_plus_placeholders": 0 }),
+        (16, {"1_to_5_placeholders": 0, "6_to_10_placeholders": 0, "11_to_15_placeholders": 0, "16_to_20_placeholders": 1, "21_plus_placeholders": 0 }),
+        (21, {"1_to_5_placeholders": 0, "6_to_10_placeholders": 0, "11_to_15_placeholders": 0, "16_to_20_placeholders": 0, "21_plus_placeholders": 1 })
+    ])
+    def test_count_of_placeholders(self, placeholders, output):
+
+        self.LOG.info(f"placeholders: {placeholders}")
+
+        # Arrange
+        index_name, index = self.create_index()
+
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "1"
+
+        try:
+            random_conversation_id = f"test_placeholder_graph_{uuid.uuid4()}"
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=random_conversation_id,
+                        registration_event_datetime=create_date_time(
+                            date=report_start, time="05:00:00"),
+                        event_type=EventType.EHR_RESPONSES.value,
+                        payload=create_ehr_response_payload(
+                            number_of_placeholders=placeholders)
+                    )),
+                sourcetype="myevent")
+            
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=random_conversation_id,
+                        registration_event_datetime=create_date_time(
+                            date=report_start, time="05:30:00"),
+                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value                        
+                    )),
+                sourcetype="myevent")
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_placeholder_snapshot_report/gp2gp_placeholders_breakdown_graph_count')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$cutoff$": cutoff
+            })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            expected_values = output
+            self.LOG.info(f"output: {output}")
+
+            for row, row_values in expected_values.items():
+                assert jq.first(
+                    f'.[] | select( ."{row}"=="{row_values}" )', telemetry)
+
+        finally:
+            self.delete_index(index_name)
+
+    @pytest.mark.parametrize("placeholders,output", [
+        (5, {"1_to_5_placeholders": "100.00", "6_to_10_placeholders": "0.00", "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "0.00" }),
+        (6, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "100.00", "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "0.00" }),
+        (11, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "0.00", "11_to_15_placeholders": "100.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "0.00" }),
+        (16, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "0.00", "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "100.00", "21_plus_placeholders": "0.00" }),
+        (21, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "0.00", "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "100.00" })
+    ])
+    def test_percentage_of_placeholders(self, placeholders, output):
+
+        self.LOG.info(f"placeholders: {placeholders}")
+
+        # Arrange
+        index_name, index = self.create_index()
+
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "1"
+
+        try:
+            random_conversation_id = f"test_placeholder_graph_{uuid.uuid4()}"
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=random_conversation_id,
+                        registration_event_datetime=create_date_time(
+                            date=report_start, time="05:00:00"),
+                        event_type=EventType.EHR_RESPONSES.value,
+                        payload=create_ehr_response_payload(
+                            number_of_placeholders=placeholders)
+                    )),
+                sourcetype="myevent")
+            
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=random_conversation_id,
+                        registration_event_datetime=create_date_time(
+                            date=report_start, time="05:30:00"),
+                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value                        
+                    )),
+                sourcetype="myevent")
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_placeholder_snapshot_report/gp2gp_placeholders_breakdown_graph_percentages')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$cutoff$": cutoff
+            })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            expected_values = output
+            self.LOG.info(f"output: {output}")
+
+            for row, row_values in expected_values.items():
+                assert jq.first(
+                    f'.[] | select( ."{row}"=="{row_values}" )', telemetry)
+
+        finally:
+            self.delete_index(index_name)
