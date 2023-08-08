0de7fdef95a90c43958eca9975c6af9f6b8a7518
LFecci <lorenzo.fecci1@nhs.net>
2023-08-08 13:28:02 +0100

    Fixed test_count_of_placeholders

--- tests/test_placeholder_snapshot_report/test_placeholder_breakdown_graph.py
+++ tests/test_placeholder_snapshot_report/test_placeholder_breakdown_graph.py
@@ -9,16 +9,17 @@ from tests.test_base import TestBase, EventType
 from helpers.datetime_helper import create_date_time, generate_report_start_date, generate_report_end_date
 import uuid
 
+
 class TestPlaceholderBreakdownGraph(TestBase):
 
-    @pytest.mark.parametrize("placeholders,output", [
-        (5, {"1_to_5_placeholders": 1, "6_to_10_placeholders": 0, "11_to_15_placeholders": 0, "16_to_20_placeholders": 0, "21_plus_placeholders": 0 }),
-        (6, {"1_to_5_placeholders": 0, "6_to_10_placeholders": 1, "11_to_15_placeholders": 0, "16_to_20_placeholders": 0, "21_plus_placeholders": 0 }),
-        (11, {"1_to_5_placeholders": 0, "6_to_10_placeholders": 0, "11_to_15_placeholders": 1, "16_to_20_placeholders": 0, "21_plus_placeholders": 0 }),
-        (16, {"1_to_5_placeholders": 0, "6_to_10_placeholders": 0, "11_to_15_placeholders": 0, "16_to_20_placeholders": 1, "21_plus_placeholders": 0 }),
-        (21, {"1_to_5_placeholders": 0, "6_to_10_placeholders": 0, "11_to_15_placeholders": 0, "16_to_20_placeholders": 0, "21_plus_placeholders": 1 })
+    @pytest.mark.parametrize("placeholders,expected_output", [
+        (5, {"1-5 placeholders": 1, "6-10 placeholders": 0, "11-15 placeholders": 0, "16-20 placeholders": 0, "21+ placeholders": 0}),
+        (6, {"1-5 placeholders": 0, "6-10 placeholders": 1, "11-15 placeholders": 0, "16-20 placeholders": 0, "21+ placeholders": 0}),
+        (11, {"1-5 placeholders": 0, "6-10 placeholders": 0, "11-15 placeholders": 1, "16-20 placeholders": 0, "21+ placeholders": 0}),
+        (16, {"1-5 placeholders": 0, "6-10 placeholders": 0, "11-15 placeholders": 0, "16-20 placeholders": 1, "21+ placeholders": 0}),
+        (21, {"1-5 placeholders": 0, "6-10 placeholders": 0, "11-15 placeholders": 0, "16-20 placeholders": 0, "21+ placeholders": 1}),
     ])
-    def test_count_of_placeholders(self, placeholders, output):
+    def test_count_of_placeholders(self, placeholders, expected_output):
 
         self.LOG.info(f"placeholders: {placeholders}")
 
@@ -43,14 +44,14 @@ class TestPlaceholderBreakdownGraph(TestBase):
                             number_of_placeholders=placeholders)
                     )),
                 sourcetype="myevent")
-            
+
             index.submit(
                 json.dumps(
                     create_sample_event(
                         conversation_id=random_conversation_id,
                         registration_event_datetime=create_date_time(
                             date=report_start, time="05:30:00"),
-                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value                        
+                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value
                     )),
                 sourcetype="myevent")
 
@@ -69,25 +70,34 @@ class TestPlaceholderBreakdownGraph(TestBase):
 
             telemetry = get_telemetry_from_splunk(
                 self.savedsearch(test_query), self.splunk_service)
+            
             self.LOG.info(f'telemetry: {telemetry}')
 
             # Assert
-            expected_values = output
-            self.LOG.info(f"output: {output}")
+            expected_values = expected_output
+            self.LOG.info(f"output: {expected_output}")            
 
-            for row, row_values in expected_values.items():
+         
+            for idx, (key, value) in enumerate(expected_values.items()):
+                self.LOG.info(f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")')
                 assert jq.first(
-                    f'.[] | select( ."{row}"=="{row_values}" )', telemetry)
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")', telemetry)
+              
 
         finally:
             self.delete_index(index_name)
 
     @pytest.mark.parametrize("placeholders,output", [
-        (5, {"1_to_5_placeholders": "100.00", "6_to_10_placeholders": "0.00", "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "0.00" }),
-        (6, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "100.00", "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "0.00" }),
-        (11, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "0.00", "11_to_15_placeholders": "100.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "0.00" }),
-        (16, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "0.00", "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "100.00", "21_plus_placeholders": "0.00" }),
-        (21, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "0.00", "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "100.00" })
+        (5, {"1_to_5_placeholders": "100.00", "6_to_10_placeholders": "0.00",
+         "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "0.00"}),
+        (6, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "100.00",
+         "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "0.00"}),
+        (11, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "0.00",
+         "11_to_15_placeholders": "100.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "0.00"}),
+        (16, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "0.00", "11_to_15_placeholders": "0.00",
+         "16_to_20_placeholders": "100.00", "21_plus_placeholders": "0.00"}),
+        (21, {"1_to_5_placeholders": "0.00", "6_to_10_placeholders": "0.00",
+         "11_to_15_placeholders": "0.00", "16_to_20_placeholders": "0.00", "21_plus_placeholders": "100.00"})
     ])
     def test_percentage_of_placeholders(self, placeholders, output):
 
@@ -114,14 +124,14 @@ class TestPlaceholderBreakdownGraph(TestBase):
                             number_of_placeholders=placeholders)
                     )),
                 sourcetype="myevent")
-            
+
             index.submit(
                 json.dumps(
                     create_sample_event(
                         conversation_id=random_conversation_id,
                         registration_event_datetime=create_date_time(
                             date=report_start, time="05:30:00"),
-                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value                        
+                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value
                     )),
                 sourcetype="myevent")
 
