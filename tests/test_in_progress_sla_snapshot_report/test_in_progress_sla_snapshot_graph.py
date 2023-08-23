440fc31f1598b858377a66330f7e7abbd4e0af14
LFecci <lorenzo.fecci1@nhs.net>
2023-08-23 14:35:48 +0100

    Added percentage query and tests

--- tests/test_in_progress_sla_snapshot_report/test_in_progress_sla_snapshot_graph.py
+++ tests/test_in_progress_sla_snapshot_report/test_in_progress_sla_snapshot_graph.py
@@ -29,7 +29,7 @@ from tests.test_base import TestBase, EventType
 
 
 class TestSnapshotInProgressSlaGraph(TestBase):
-    def test_in_flight(self):
+    def test_in_flight_count(self):
         # Arrange
         index_name, index = self.create_index()
 
@@ -154,7 +154,132 @@ class TestSnapshotInProgressSlaGraph(TestBase):
         finally:
             self.delete_index(index_name)
 
-    def test_broken_24hr_sla(self):
+    def test_in_flight_percentage(self):
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = date.today()
+        report_end = generate_report_end_date()
+        cutoff = "0"
+
+        try:
+            in_flight_conversation_id = "test_in_flight"
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=in_flight_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(minutes=6)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=in_flight_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(minutes=5)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.EHR_REQUESTS.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test_in_flight",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=in_flight_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(minutes=4)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.EHR_RESPONSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=in_flight_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(minutes=3)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                "gp2gp_in_progress_sla_snapshot_report/gp2gp_in_progress_sla_snapshot_report_percentage"
+            )
+
+            test_query = set_variables_on_query(
+                test_query,
+                {
+                    "$index$": index_name,
+                    "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$cutoff$": cutoff,
+                },
+            )
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service
+            )
+            self.LOG.info(f"telemetry: {telemetry}")
+
+            # Assert
+
+            expected_values = {
+                "In flight": "100.00",
+                "Broken 24hr sla": "0.00",
+                "Broken ehr sending sla": "0.00",
+                "Broken ehr requesting sla": "0.00",
+            }
+
+            for idx, (key, value) in enumerate(expected_values.items()):
+                self.LOG.info(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")'
+                )
+                assert jq.first(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")',
+                    telemetry,
+                )
+
+        finally:
+            self.delete_index(index_name)
+
+    def test_broken_24hr_sla_count(self):
         # Arrange
         index_name, index = self.create_index()
 
@@ -279,7 +404,132 @@ class TestSnapshotInProgressSlaGraph(TestBase):
         finally:
             self.delete_index(index_name)
 
-    def test_broken_ehr_sending_sla(self):
+    def test_broken_24hr_sla_percentage(self):
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "0"
+
+        try:
+            broken_24hr_sla_conversation_id = "test_broken_24hr_sla"
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=broken_24hr_sla_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(hours=25)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=broken_24hr_sla_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(hours=25)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.EHR_REQUESTS.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test_in_flight",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=broken_24hr_sla_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(hours=25)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.EHR_RESPONSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=broken_24hr_sla_conversation_id,
+                        registration_event_datetime=(datetime_utc_now()).strftime(
+                            "%Y-%m-%dT%H:%M:%S%z"
+                        ),
+                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                "gp2gp_in_progress_sla_snapshot_report/gp2gp_in_progress_sla_snapshot_report_percentage"
+            )
+
+            test_query = set_variables_on_query(
+                test_query,
+                {
+                    "$index$": index_name,
+                    "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$cutoff$": cutoff,
+                },
+            )
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service
+            )
+            self.LOG.info(f"telemetry: {telemetry}")
+
+            # Assert
+
+            expected_values = {
+                "In flight": "0.00",
+                "Broken 24hr sla": "100.00",
+                "Broken ehr sending sla": "0.00",
+                "Broken ehr requesting sla": "0.00",
+            }
+
+            for idx, (key, value) in enumerate(expected_values.items()):
+                self.LOG.info(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")'
+                )
+                assert jq.first(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")',
+                    telemetry,
+                )
+
+        finally:
+            self.delete_index(index_name)
+
+    def test_broken_ehr_sending_sla_count(self):
         # Arrange
         index_name, index = self.create_index()
 
@@ -391,7 +641,119 @@ class TestSnapshotInProgressSlaGraph(TestBase):
         finally:
             self.delete_index(index_name)
 
-    def test_broken_ehr_requesting_sla(self):
+    def test_broken_ehr_sending_sla_percentage(self):
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "0"
+
+        try:
+            broken_ehr_sending_outside_sla_conversation_id = (
+                "test_broken_ehr_sending_outside_sla"
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=broken_ehr_sending_outside_sla_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(minutes=25)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=broken_ehr_sending_outside_sla_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(minutes=21)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.EHR_REQUESTS.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test_in_flight",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=broken_ehr_sending_outside_sla_conversation_id,
+                        registration_event_datetime=(datetime_utc_now()).strftime(
+                            "%Y-%m-%dT%H:%M:%S%z"
+                        ),
+                        event_type=EventType.EHR_RESPONSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                "gp2gp_in_progress_sla_snapshot_report/gp2gp_in_progress_sla_snapshot_report_percentage"
+            )
+
+            test_query = set_variables_on_query(
+                test_query,
+                {
+                    "$index$": index_name,
+                    "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$cutoff$": cutoff,
+                },
+            )
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service
+            )
+            self.LOG.info(f"telemetry: {telemetry}")
+
+            # Assert
+
+            expected_values = {
+                "In flight": "0.00",
+                "Broken 24hr sla": "0.00",
+                "Broken ehr sending sla": "100.00",
+                "Broken ehr requesting sla": "0.00",
+            }
+
+            for idx, (key, value) in enumerate(expected_values.items()):
+                self.LOG.info(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")'
+                )
+                assert jq.first(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")',
+                    telemetry,
+                )
+
+        finally:
+            self.delete_index(index_name)
+
+    def test_broken_ehr_requesting_sla_count(self):
         # Arrange
         index_name, index = self.create_index()
 
@@ -467,6 +829,96 @@ class TestSnapshotInProgressSlaGraph(TestBase):
                 "Broken 24hr sla": "0",
                 "Broken ehr sending sla": "0",
                 "Broken ehr requesting sla": "1",
+            }
+
+            for idx, (key, value) in enumerate(expected_values.items()):
+                self.LOG.info(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")'
+                )
+                assert jq.first(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")',
+                    telemetry,
+                )
+
+        finally:
+            self.delete_index(index_name)
+
+    def test_broken_ehr_requesting_sla_percentage(self):
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "0"
+
+        try:
+            broken_ehr_requesting_sla_conversation_id = "broken_ehr_requesting_sla"
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=broken_ehr_requesting_sla_conversation_id,
+                        registration_event_datetime=(
+                            datetime_utc_now() - timedelta(hours=1)
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id=broken_ehr_requesting_sla_conversation_id,
+                        registration_event_datetime=(datetime_utc_now()).strftime(
+                            "%Y-%m-%dT%H:%M:%S%z"
+                        ),
+                        event_type=EventType.EHR_REQUESTS.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                "gp2gp_in_progress_sla_snapshot_report/gp2gp_in_progress_sla_snapshot_report_percentage"
+            )
+
+            test_query = set_variables_on_query(
+                test_query,
+                {
+                    "$index$": index_name,
+                    "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$cutoff$": cutoff,
+                },
+            )
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service
+            )
+            self.LOG.info(f"telemetry: {telemetry}")
+
+            # Assert
+           
+            expected_values = {
+                "In flight": "0.00",
+                "Broken 24hr sla": "0.00",
+                "Broken ehr sending sla": "0.00",
+                "Broken ehr requesting sla": "100.00",
             }
 
             for idx, (key, value) in enumerate(expected_values.items()):
