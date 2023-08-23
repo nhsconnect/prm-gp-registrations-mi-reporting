20bea3cc38ef2f781d588ef74959871c36eb1888
LFecci <lorenzo.fecci1@nhs.net>
2023-08-23 15:28:25 +0100

    Added total number of records

--- tests/test_in_progress_sla_snapshot_report/test_in_progress_sla_snapshot_graph.py
+++ tests/test_in_progress_sla_snapshot_report/test_in_progress_sla_snapshot_graph.py
@@ -932,3 +932,108 @@ class TestSnapshotInProgressSlaGraph(TestBase):
 
         finally:
             self.delete_index(index_name)
+
+
+        
+    def test_total_records_in_progress(self):
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "0"
+
+        try:
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id="not_transfer_compatable",
+                        registration_event_datetime=(
+                            datetime_utc_now()
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=False,
+                            reason="test",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id="transfer_compatable_1",
+                        registration_event_datetime=(
+                            datetime_utc_now()
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id="transfer_compatable_2",
+                        registration_event_datetime=(
+                            datetime_utc_now()
+                        ).strftime("%Y-%m-%dT%H:%M:%S%z"),
+                        event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
+                        sendingSupplierName="EMIS",
+                        requestingSupplierName="TPP",
+                        payload=create_transfer_compatibility_payload(
+                            internalTransfer=False,
+                            transferCompatible=True,
+                            reason="test",
+                        ),
+                    )
+                ),
+                sourcetype="myevent",
+            )
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                "gp2gp_in_progress_sla_snapshot_report/gp2gp_in_progress_sla_snapshot_report_total_num_records_in_progress"
+            )
+
+            test_query = set_variables_on_query(
+                test_query,
+                {
+                    "$index$": index_name,
+                    "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                    "$cutoff$": cutoff,
+                },
+            )
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service
+            )
+            self.LOG.info(f"telemetry: {telemetry}")
+
+            # Assert
+
+            assert jq.first(
+                    f'.[] | select( .totalRecords=="2")',
+                    telemetry,
+                )
+
+        finally:
+            self.delete_index(index_name)
