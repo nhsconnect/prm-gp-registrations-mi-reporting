a0be0a0c54a790864f8dfc8b83227355b06bc30d
LFecci <lorenzo.fecci1@nhs.net>
2023-08-30 17:32:54 +0100

    WIP faker implementation

--- faker_test.py
+++ faker_test.py
@@ -1,8 +1,11 @@
-from tests.faker.providers.events.registration_factory import RegistrationEvent
+from tests.faker.providers.events.event_factory import RegistrationEvent, ReadyToIntegrateStatuses
 
-
+# RegistrationEvent
 re = RegistrationEvent()
+re_json = re.get_json()
+print(f"RegistrationEvent Json: {re_json}")
 
-json = re.get_json()
-
-print(f"Json: {json}")
+# # ReadyToIntegrateStatuses
+# rti = ReadyToIntegrateStatuses()
+# rti_json = rti.get_json();
+# print(f"ReadyToIntegrateStatuses Json: {rti_json}")
