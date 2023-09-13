db5ed1609e0324fdd2754571cdddb7fd1d02540b
JoeSykesNHS <joe.sykes1@nhs.net>
2023-09-13 15:35:38 +0100

    [JS] Generated and tested query for successfully integrated count graph and percentage graph.

--- tests/test_transfer_status_snapshot_report/test_transfer_status_report_snapshot_outputs.py
+++ tests/test_transfer_status_snapshot_report/test_transfer_status_report_snapshot_outputs.py
@@ -293,3 +293,131 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
 
         finally:
             self.delete_index(index_name)
+
+    def test_gp2gp_transfer_status_report_snapshot_successfully_integrated_count(self):
+
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "0"
+
+        try:
+            different_outcomes_list = ["INTEGRATED",
+                                       "INTEGRATED_AND_SUPPRESSED",
+                                       "SUPPRESSED_AND_REACTIVATED",
+                                       "FILED_AS_ATTACHMENT",
+                                       "INTERNAL_TRANSFER",
+                                       "REJECTED"
+                                       ]
+
+            for outcome in different_outcomes_list:
+                index.submit(
+                    json.dumps(
+                        create_sample_event(
+                            f'ehr_integrations_{outcome}',
+                            registration_event_datetime=create_date_time(report_start, "08:00:00"),
+                            event_type=EventType.EHR_INTEGRATIONS.value,
+                            sendingSupplierName="EMIS",
+                            requestingSupplierName="TPP",
+                            payload=create_integration_payload(
+                                outcome=outcome
+                            )
+
+                        )),
+                    sourcetype="myevent")
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_transfer_status_snapshot_report/'
+                'gp2gp_transfer_status_report_snapshot_successfully_integrated_count')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$cutoff$": cutoff
+            })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            expected_values = {"Successfully integrated": "5",
+                               "Not successfully integrated": "1"}
+
+            for idx, (key, value) in enumerate(expected_values.items()):
+                assert jq.first(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")', telemetry)
+
+        finally:
+            self.delete_index(index_name)
+
+    def test_gp2gp_transfer_status_report_snapshot_successfully_integrated_percentage(self):
+
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "0"
+
+        try:
+            different_outcomes_list = ["INTEGRATED",
+                                       "INTEGRATED_AND_SUPPRESSED",
+                                       "SUPPRESSED_AND_REACTIVATED",
+                                       "FILED_AS_ATTACHMENT",
+                                       "INTERNAL_TRANSFER",
+                                       "REJECTED"
+                                       ]
+
+            for outcome in different_outcomes_list:
+                index.submit(
+                    json.dumps(
+                        create_sample_event(
+                            f'ehr_integrations_{outcome}',
+                            registration_event_datetime=create_date_time(report_start, "08:00:00"),
+                            event_type=EventType.EHR_INTEGRATIONS.value,
+                            sendingSupplierName="EMIS",
+                            requestingSupplierName="TPP",
+                            payload=create_integration_payload(
+                                outcome=outcome
+                            )
+
+                        )),
+                    sourcetype="myevent")
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_transfer_status_snapshot_report/'
+                'gp2gp_transfer_status_report_snapshot_successfully_integrated_percentage')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$cutoff$": cutoff
+            })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            expected_values = {"Successfully integrated": "83.33",
+                               "Not successfully integrated": "16.67"}
+
+            for idx, (key, value) in enumerate(expected_values.items()):
+                assert jq.first(
+                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")', telemetry)
+
+        finally:
+            self.delete_index(index_name)
