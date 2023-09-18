f43ecd3d3c6b9b22fea414cd8991f3567b70498e
JoeSykesNHS <127425466+JoeSykesNHS@users.noreply.github.com>
2023-09-18 11:59:18 +0100

    Fix transfer status snapshot report (#33)
    * [JS] fixed counts so that all columns add up to total number of eligible transfers.
    
    * [JS] fixed counts test.
    
    * [JS] fixed percentages query and test.
    
    * [JS] fixed total eligible for transfer value query.
    
    * [JS] fixed successfully integrated count graph query.
    
    * [JS] fixed successfully integrated percentages graph query.
    
    * [JS] Refactored successfully integrated tests and queries into their separate folder to follow file structure for separate dashboards.

---
+++ tests/test_successfully_integrated_snapshot_report/test_successfully_integrated_report_outputs.py
@@ -0,0 +1,158 @@
+import json
+import pytest
+from time import sleep
+import jq
+from helpers.splunk \
+    import get_telemetry_from_splunk, create_sample_event, set_variables_on_query, \
+    create_integration_payload
+from tests.test_base import TestBase, EventType
+from helpers.datetime_helper import create_date_time, generate_report_start_date, \
+    generate_report_end_date
+
+
+class TestSuccessfullyIntegratedSnapshotReportOutputs(TestBase):
+
+    def test_gp2gp_successfully_integrated_report_snapshot_count(self):
+
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "0"
+
+        try:
+            different_outcomes_list = ["INTEGRATED",
+                                       "INTEGRATED_AND_SUPPRESSED",
+                                       "SUPPRESSED_AND_REACTIVATED",
+                                       "FILED_AS_ATTACHMENT",
+                                       "INTERNAL_TRANSFER",
+                                       "REJECTED"
+                                       ]
+
+            for outcome in different_outcomes_list:
+                index.submit(
+                    json.dumps(
+                        create_sample_event(
+                            f'ehr_integrations_{outcome}',
+                            registration_event_datetime=create_date_time(report_start, "08:00:00"),
+                            event_type=EventType.EHR_INTEGRATIONS.value,
+                            sendingSupplierName="EMIS",
+                            requestingSupplierName="TPP",
+                            payload=create_integration_payload(
+                                outcome=outcome
+                            )
+
+                        )),
+                    sourcetype="myevent")
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_successfully_integrated_snapshot_report/'
+                'gp2gp_transfer_status_report_snapshot_successfully_integrated_count')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$cutoff$": cutoff
+            })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            expected_values = {"0": {"column": "Not successfully integrated",
+                                     "count": "1"},
+                               "1": {"column": "Successfully integrated",
+                                     "count": "5"},
+                               }
+
+            for row, row_values in expected_values.items():
+                row_values_as_jq_str = ' '.join(
+                    [f"| select(.\"{key}\"==\"{value}\") " for key, value in row_values.items()]
+                )
+                self.LOG.info(f'.[{row}] {row_values_as_jq_str} ')
+                assert jq.first(
+                    f'.[{row}] {row_values_as_jq_str} ', telemetry)
+
+        finally:
+            self.delete_index(index_name)
+
+    def test_gp2gp_successfully_integrated_report_snapshot_percentage(self):
+
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = generate_report_start_date()
+        report_end = generate_report_end_date()
+        cutoff = "0"
+
+        try:
+            different_outcomes_list = ["INTEGRATED",
+                                       "INTEGRATED_AND_SUPPRESSED",
+                                       "SUPPRESSED_AND_REACTIVATED",
+                                       "FILED_AS_ATTACHMENT",
+                                       "INTERNAL_TRANSFER",
+                                       "REJECTED"
+                                       ]
+
+            for outcome in different_outcomes_list:
+                index.submit(
+                    json.dumps(
+                        create_sample_event(
+                            f'ehr_integrations_{outcome}',
+                            registration_event_datetime=create_date_time(report_start, "08:00:00"),
+                            event_type=EventType.EHR_INTEGRATIONS.value,
+                            sendingSupplierName="EMIS",
+                            requestingSupplierName="TPP",
+                            payload=create_integration_payload(
+                                outcome=outcome
+                            )
+
+                        )),
+                    sourcetype="myevent")
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_successfully_integrated_snapshot_report/'
+                'gp2gp_transfer_status_report_snapshot_successfully_integrated_percentage')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
+                "$cutoff$": cutoff
+            })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            expected_values = {"Successfully integrated": "83.33",
+                               "Not successfully integrated": "16.67"}
+
+            expected_values = {"0": {"column": "Not successfully integrated",
+                                     "count": "16.67"},
+                               "1": {"column": "Successfully integrated",
+                                     "count": "83.33"},
+                               }
+
+            for row, row_values in expected_values.items():
+                row_values_as_jq_str = ' '.join(
+                    [f"| select(.\"{key}\"==\"{value}\") " for key, value in row_values.items()]
+                )
+                self.LOG.info(f'.[{row}] {row_values_as_jq_str} ')
+                assert jq.first(
+                    f'.[{row}] {row_values_as_jq_str} ', telemetry)
+
+        finally:
+            self.delete_index(index_name)
--- tests/test_transfer_status_snapshot_report/test_transfer_status_report_snapshot_outputs.py
+++ tests/test_transfer_status_snapshot_report/test_transfer_status_report_snapshot_outputs.py
@@ -5,11 +5,12 @@ from time import sleep
 from splunklib import client
 import jq
 from helpers.splunk \
-    import get_telemetry_from_splunk,  create_sample_event, set_variables_on_query, \
+    import get_telemetry_from_splunk, create_sample_event, set_variables_on_query, \
     create_integration_payload, create_transfer_compatibility_payload
 from tests.test_base import TestBase, EventType
 from datetime import datetime, timedelta
-from helpers.datetime_helper import datetime_utc_now, create_date_time, generate_report_start_date, generate_report_end_date
+from helpers.datetime_helper import datetime_utc_now, create_date_time, generate_report_start_date, \
+    generate_report_end_date
 
 
 class TestTransferStatusReportSnapshotOutputs(TestBase):
@@ -20,7 +21,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
         # Arrange
         index_name, index = self.create_index()
 
-         # reporting window
+        # reporting window
         report_start = generate_report_start_date()
         report_end = generate_report_end_date()
         cutoff = "0"
@@ -31,7 +32,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 json.dumps(
                     create_sample_event(
                         'test_total_eligible_for_electronic_transfer_1',
-                        registration_event_datetime=create_date_time(report_start,"08:00:00"),
+                        registration_event_datetime=create_date_time(report_start, "08:00:00"),
                         event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
                         sendingSupplierName="EMIS",
                         requestingSupplierName="TPP",
@@ -48,7 +49,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 json.dumps(
                     create_sample_event(
                         'test_total_eligible_for_electronic_transfer_2',
-                        registration_event_datetime=create_date_time(report_start,"09:00:00"),
+                        registration_event_datetime=create_date_time(report_start, "09:00:00"),
                         event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
                         sendingSupplierName="EMIS",
                         requestingSupplierName="TPP",
@@ -65,7 +66,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 json.dumps(
                     create_sample_event(
                         'test_total_eligible_for_electronic_transfer_3',
-                        registration_event_datetime=create_date_time(report_start,"10:00:00"),
+                        registration_event_datetime=create_date_time(report_start, "10:00:00"),
                         event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
                         sendingSupplierName="EMIS",
                         requestingSupplierName="TPP",
@@ -96,16 +97,16 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
             self.LOG.info(f'telemetry: {telemetry}')
 
             # Assert
-            expected_values = {"Successfully integrated": "0",
-                               "Rejected": "0",
-                               "Awaiting integration": "0",
-                               "In progress": "2",
-                               "Technical failure": "0",
-                               "Unknown": "0"}
+            expected_values = {"0": {"outcome": "In progress",
+                                     "count": "2"}}
 
-            for idx, (key, value) in enumerate(expected_values.items()):
+            for row, row_values in expected_values.items():
+                row_values_as_jq_str = ' '.join(
+                    [f"| select(.\"{key}\"==\"{value}\") " for key, value in row_values.items()]
+                )
+                self.LOG.info(f'.[{row}] {row_values_as_jq_str} ')
                 assert jq.first(
-                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")', telemetry)
+                    f'.[{row}] {row_values_as_jq_str} ', telemetry)
 
         finally:
             self.delete_index(index_name)
@@ -116,7 +117,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
         # Arrange
         index_name, index = self.create_index()
 
-         # reporting window
+        # reporting window
         report_start = datetime.today().date().replace(day=1)
         report_end = datetime.today().date().replace(day=28)
         cutoff = "0"
@@ -127,7 +128,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 json.dumps(
                     create_sample_event(
                         'test_total_eligible_for_electronic_transfer_1',
-                        registration_event_datetime=create_date_time(report_start,"08:00:00"),
+                        registration_event_datetime=create_date_time(report_start, "08:00:00"),
                         event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
                         sendingSupplierName="EMIS",
                         requestingSupplierName="TPP",
@@ -144,7 +145,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 json.dumps(
                     create_sample_event(
                         'test_total_eligible_for_electronic_transfer_2',
-                        registration_event_datetime=create_date_time(report_start,"09:00:00"),
+                        registration_event_datetime=create_date_time(report_start, "09:00:00"),
                         event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
                         sendingSupplierName="EMIS",
                         requestingSupplierName="TPP",
@@ -161,7 +162,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 json.dumps(
                     create_sample_event(
                         'test_total_eligible_for_electronic_transfer_3',
-                        registration_event_datetime=create_date_time(report_start,"10:00:00"),
+                        registration_event_datetime=create_date_time(report_start, "10:00:00"),
                         event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
                         sendingSupplierName="EMIS",
                         requestingSupplierName="TPP",
@@ -172,7 +173,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                         )
 
                     )),
-                sourcetype="myevent")        
+                sourcetype="myevent")
 
             # Act
             test_query = self.generate_splunk_query_from_report(
@@ -192,22 +193,23 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
             self.LOG.info(f'telemetry: {telemetry}')
 
             # Assert
-            expected_values = {"Successfully integrated": "0.00",
-                               "Rejected": "0.00",
-                               "Awaiting integration": "0.00",
-                               "In progress": "100.00",
-                               "Technical failure": "0.00",
-                               "Unknown": "0.00"}
+            expected_values = {"0": {"outcome": "In progress",
+                                     "count": "100.00"}}
 
-            for idx, (key, value) in enumerate(expected_values.items()):
+            for row, row_values in expected_values.items():
+                row_values_as_jq_str = ' '.join(
+                    [f"| select(.\"{key}\"==\"{value}\") " for key, value in row_values.items()]
+                )
+                self.LOG.info(f'.[{row}] {row_values_as_jq_str} ')
                 assert jq.first(
-                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")', telemetry)
+                    f'.[{row}] {row_values_as_jq_str} ', telemetry)
+
 
         finally:
             self.delete_index(index_name)
 
     def test_gp2gp_transfer_status_report_snapshot_total_eligible_for_transfer(self):
-        
+
         # Arrange
         index_name, index = self.create_index()
 
@@ -222,7 +224,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 json.dumps(
                     create_sample_event(
                         'test_total_eligible_for_electronic_transfer_1',
-                        registration_event_datetime=create_date_time(report_start,"08:00:00"),
+                        registration_event_datetime=create_date_time(report_start, "08:00:00"),
                         event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
                         sendingSupplierName="EMIS",
                         requestingSupplierName="TPP",
@@ -239,7 +241,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 json.dumps(
                     create_sample_event(
                         'test_total_eligible_for_electronic_transfer_2',
-                        registration_event_datetime=create_date_time(report_start,"09:00:00"),
+                        registration_event_datetime=create_date_time(report_start, "09:00:00"),
                         event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
                         sendingSupplierName="EMIS",
                         requestingSupplierName="TPP",
@@ -256,7 +258,7 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 json.dumps(
                     create_sample_event(
                         'test_total_eligible_for_electronic_transfer_3',
-                        registration_event_datetime=create_date_time(report_start,"10:00:00"),
+                        registration_event_datetime=create_date_time(report_start, "10:00:00"),
                         event_type=EventType.TRANSFER_COMPATIBILITY_STATUSES.value,
                         sendingSupplierName="EMIS",
                         requestingSupplierName="TPP",
@@ -286,138 +288,9 @@ class TestTransferStatusReportSnapshotOutputs(TestBase):
                 self.savedsearch(test_query), self.splunk_service)
             self.LOG.info(f'telemetry: {telemetry}')
 
-
-            # Assert                
+            # Assert
             assert jq.first(
                 f'.[] | select( .total_eligible_for_electronic_transfer=="2")', telemetry)
-
-        finally:
-            self.delete_index(index_name)
-
-    def test_gp2gp_transfer_status_report_snapshot_successfully_integrated_count(self):
-
-        # Arrange
-        index_name, index = self.create_index()
-
-        # reporting window
-        report_start = generate_report_start_date()
-        report_end = generate_report_end_date()
-        cutoff = "0"
-
-        try:
-            different_outcomes_list = ["INTEGRATED",
-                                       "INTEGRATED_AND_SUPPRESSED",
-                                       "SUPPRESSED_AND_REACTIVATED",
-                                       "FILED_AS_ATTACHMENT",
-                                       "INTERNAL_TRANSFER",
-                                       "REJECTED"
-                                       ]
-
-            for outcome in different_outcomes_list:
-                index.submit(
-                    json.dumps(
-                        create_sample_event(
-                            f'ehr_integrations_{outcome}',
-                            registration_event_datetime=create_date_time(report_start, "08:00:00"),
-                            event_type=EventType.EHR_INTEGRATIONS.value,
-                            sendingSupplierName="EMIS",
-                            requestingSupplierName="TPP",
-                            payload=create_integration_payload(
-                                outcome=outcome
-                            )
-
-                        )),
-                    sourcetype="myevent")
-
-            # Act
-            test_query = self.generate_splunk_query_from_report(
-                'gp2gp_transfer_status_snapshot_report/'
-                'gp2gp_transfer_status_report_snapshot_successfully_integrated_count')
-
-            test_query = set_variables_on_query(test_query, {
-                "$index$": index_name,
-                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
-                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
-                "$cutoff$": cutoff
-            })
-
-            sleep(2)
-
-            telemetry = get_telemetry_from_splunk(
-                self.savedsearch(test_query), self.splunk_service)
-            self.LOG.info(f'telemetry: {telemetry}')
-
-            # Assert
-            expected_values = {"Successfully integrated": "5",
-                               "Not successfully integrated": "1"}
-
-            for idx, (key, value) in enumerate(expected_values.items()):
-                assert jq.first(
-                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")', telemetry)
-
-        finally:
-            self.delete_index(index_name)
-
-    def test_gp2gp_transfer_status_report_snapshot_successfully_integrated_percentage(self):
-
-        # Arrange
-        index_name, index = self.create_index()
-
-        # reporting window
-        report_start = generate_report_start_date()
-        report_end = generate_report_end_date()
-        cutoff = "0"
-
-        try:
-            different_outcomes_list = ["INTEGRATED",
-                                       "INTEGRATED_AND_SUPPRESSED",
-                                       "SUPPRESSED_AND_REACTIVATED",
-                                       "FILED_AS_ATTACHMENT",
-                                       "INTERNAL_TRANSFER",
-                                       "REJECTED"
-                                       ]
-
-            for outcome in different_outcomes_list:
-                index.submit(
-                    json.dumps(
-                        create_sample_event(
-                            f'ehr_integrations_{outcome}',
-                            registration_event_datetime=create_date_time(report_start, "08:00:00"),
-                            event_type=EventType.EHR_INTEGRATIONS.value,
-                            sendingSupplierName="EMIS",
-                            requestingSupplierName="TPP",
-                            payload=create_integration_payload(
-                                outcome=outcome
-                            )
-
-                        )),
-                    sourcetype="myevent")
-
-            # Act
-            test_query = self.generate_splunk_query_from_report(
-                'gp2gp_transfer_status_snapshot_report/'
-                'gp2gp_transfer_status_report_snapshot_successfully_integrated_percentage')
-
-            test_query = set_variables_on_query(test_query, {
-                "$index$": index_name,
-                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%M:%S%z"),
-                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%M:%S%z"),
-                "$cutoff$": cutoff
-            })
-
-            sleep(2)
-
-            telemetry = get_telemetry_from_splunk(
-                self.savedsearch(test_query), self.splunk_service)
-            self.LOG.info(f'telemetry: {telemetry}')
-
-            # Assert
-            expected_values = {"Successfully integrated": "83.33",
-                               "Not successfully integrated": "16.67"}
-
-            for idx, (key, value) in enumerate(expected_values.items()):
-                assert jq.first(
-                    f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")', telemetry)
 
         finally:
             self.delete_index(index_name)
