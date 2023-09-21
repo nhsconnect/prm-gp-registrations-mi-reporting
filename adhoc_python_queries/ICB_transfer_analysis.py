91e5b7b1f32db088d2b63e01baad8079f9ad3180
JoeSykesNHS <127425466+JoeSykesNHS@users.noreply.github.com>
2023-09-21 14:50:16 +0100

    Prmt 3637 successfully integrated raw data table (#36)
    * [JS] changed query to look at de-registrations from BSLO ICB for 6 months.
    
    * [JS] deleted unnecessary files.
    
    * [JS] Created query and tests for raw data table.
    
    * [JS] Edited dashboard to show raw data table.

--- adhoc_python_queries/ICB_transfer_analysis.py
+++ adhoc_python_queries/ICB_transfer_analysis.py
@@ -1,23 +1,24 @@
 import pandas as pd
 
 final_df = pd.DataFrame()
-date_dict = {"06": 30, "07": 31, "08": 31}
+date_dict = {"03": 31, "04": 30, "05": 31, "06": 30, "07": 31, "08": 31}
 
 for month, num_days in date_dict.items():
-    for i in range(1, num_days+1):
+    for i in range(1, num_days + 1):
         day = "0" + str(i) if i < 10 else str(i)
         path = f"~/2023/{month}/{day}/2023-{month}-{day}-transfers.parquet"
         df_raw = pd.read_parquet(path, engine="pyarrow")
 
         df_icb = df_raw[["conversation_id",
-                         "sending_practice_ods_code",
+                         # "sending_practice_ods_code",
                          "requesting_practice_sicbl_ods_code",
                          "requesting_practice_sicbl_name",
                          "sending_practice_sicbl_ods_code",
                          "sending_practice_sicbl_name"
                          ]]
-        df_icb = df_icb[df_icb["sending_practice_ods_code"] == "M85143"]
-        del df_icb["sending_practice_ods_code"]
+        # df_icb = df_icb[df_icb["sending_practice_ods_code"] == "M85143"]
+        df_icb = df_icb[df_icb["sending_practice_sicbl_ods_code"] == "15E"]
+        # del df_icb["sending_practice_ods_code"]
 
         final_df = pd.concat([final_df, df_icb])
 
@@ -32,4 +33,5 @@ final_df = final_df.groupby(["requesting_practice_sicbl_ods_code",
 
 final_df = final_df.sort_values(by=["conversation_id"], ascending=False).reset_index()
 final_df = final_df.rename(columns={"conversation_id": "num_leaving_patients"})
-print(final_df.to_markdown())
+final_df.to_csv(path_or_buf="adhoc_python_queries/ICB_transfer_analysis_BSOL_ICB.csv", index=False)
+# print(final_df.to_markdown())
