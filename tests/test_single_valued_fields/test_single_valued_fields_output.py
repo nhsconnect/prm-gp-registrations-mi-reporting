f39d331fbe44d8f457abe8befeb2386a7a1a5963
Renny Fadoju <146440516+RennyFadoju@users.noreply.github.com>
2023-11-23 12:06:35 +0000

    Add single value field query for total successfully integrated records

--- tests/test_single_valued_fields/test_single_valued_fields_output.py
+++ tests/test_single_valued_fields/test_single_valued_fields_output.py
@@ -196,15 +196,14 @@ class TestSingleValuedFieldOutputs(TestBase):
 
     def test_gp2gp_single_valued_field_total_records_transferred(self):
 
+        index_name, index = self.create_index()
+
         # reporting window
         report_start = datetime.today().date().replace(day=1)
         report_end = datetime.today().date().replace(day=28)
         cutoff = "0"
 
         try:
-            # Arrange
-            index_name, index = self.create_index()
-
             index.submit(
                 json.dumps(
                     create_sample_event(
@@ -278,6 +277,8 @@ class TestSingleValuedFieldOutputs(TestBase):
             self.delete_index(index_name)
 
     def test_gp2gp_single_valued_field_total_awaiting_integration(self):
+        # Arrange
+        index_name, index = self.create_index()
 
         # reporting window
         report_start = datetime.today().date().replace(day=1)
@@ -285,9 +286,6 @@ class TestSingleValuedFieldOutputs(TestBase):
         cutoff = "0"
 
         try:
-            # Arrange
-            index_name, index = self.create_index()
-
             index.submit(
                 json.dumps(
                     create_sample_event(
@@ -360,6 +358,80 @@ class TestSingleValuedFieldOutputs(TestBase):
 
             # Assert
             expected_values = {"total_awaiting_integration": "1"}
+
+            for idx, (key, value) in enumerate(expected_values.items()):
+                self.LOG.info(f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")')
+                assert jq.first(
+                    f'.[{idx}] | select( .{key}=="{value}")', telemetry)
+
+        finally:
+            self.delete_index(index_name)
+
+    def test_gp2gp_single_valued_field_total_successfully_integrated(self):
+        # Arrange
+        index_name, index = self.create_index()
+
+        # reporting window
+        report_start = datetime.today().date().replace(day=1)
+        report_end = datetime.today().date().replace(day=28)
+        cutoff = "0"
+
+        try:
+            different_outcomes_list = [
+                "INTEGRATED",
+                "INTEGRATED_AND_SUPPRESSED",
+                "SUPPRESSED_AND_REACTIVATED",
+                "FILED_AS_ATTACHMENT",
+                "INTERNAL_TRANSFER",
+                "REJECTED",
+                "FAILED_TO_INTEGRATE"
+            ]
+
+            for outcome in different_outcomes_list:
+                index.submit(
+                    json.dumps(
+                        create_sample_event(
+                            f'ehr_integrations_{outcome}',
+                            registration_event_datetime=create_date_time(report_start, "08:00:00"),
+                            event_type=EventType.EHR_INTEGRATIONS.value,
+                            sendingSupplierName="EMIS",
+                            requestingSupplierName="TPP",
+                            payload=create_integration_payload(
+                                outcome=outcome
+                            )
+
+                        )),
+                    sourcetype="myevent")
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_awaiting_integration',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:00:00"),
+                        event_type=EventType.READY_TO_INTEGRATE_STATUSES.value
+                    )),
+                sourcetype="myevent")
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_single_valued_fields/'
+                'gp2gp_total_successfully_integrated_count')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$cutoff$": cutoff
+            })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            expected_values = {"total_successfully_integrated": "5"}
 
             for idx, (key, value) in enumerate(expected_values.items()):
                 self.LOG.info(f'.[{idx}] | select( .label=="{key}") | select (.count=="{value}")')
