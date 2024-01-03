4d4799168a6149d79c13094b40bec0837e96047c
JoeSykesNHS <joe.sykes1@nhs.net>
2024-01-03 12:08:03 +0000

    [JS] Corrected failure point count and percentage graph to deal with multiple errors per conv

--- tests/test_technical_failures_snapshot_report/test_technical_failures_snapshot_report_outputs.py
+++ tests/test_technical_failures_snapshot_report/test_technical_failures_snapshot_report_outputs.py
@@ -800,6 +800,148 @@ class TestTechnicalFailuresSnapshotReportOutputs(TestBase):
         finally:
             self.delete_index(index_name)
 
+    @pytest.mark.parametrize("clicked_column, expected_val",
+                             [("06", {"failure_point": "EHR Response", "count": "1"}),
+                              ("09", {"failure_point": "Other", "count": "1"}),
+                              ("Integration failure", {"failure_point": "EHR_INTEGRATION", "count": "1"})])
+    def test_gp2gp_technical_failures_snapshot_report_failure_point_graph_count_2_failure_points_1_conv(self,
+                                                                                                        clicked_column,
+                                                                                                        expected_val):
+
+        # reporting window
+        report_start = datetime.today().date().replace(day=1)
+        report_end = datetime.today().date().replace(day=28)
+        cutoff = "0"
+
+        try:
+            # Arrange
+            index_name, index = self.create_index()
+
+            # order needs to be alphabetical for test to pass as chart orders columns in this way
+            failure_points_and_amounts = [
+                ("EHR Ready to Integrate", 3),
+                ("EHR Requested", 2),
+                ("EHR Response", 1),
+                ("Endpoint Lookup", 3),
+                ("Other", 2),
+                ("Patient General Update", 4),
+                ("Patient Trace", 1)
+            ]
+
+            expected_vals = OrderedDict()
+
+            # error code 06
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_multiple_failure_points',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:00:00"),
+                        event_type=EventType.EHR_REQUESTS.value
+                    )),
+                sourcetype="myevent")
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_multiple_failure_points',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:01:00"),
+                        event_type=EventType.ERRORS.value,
+                        payload=create_error_payload(
+                            errorCode="06",
+                            errorDescription="Random desc",
+                            failurePoint="EHR Response"
+                        )
+
+                    )),
+                sourcetype="myevent")
+
+            # FAILED_TO_INTEGRATE
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_failure_point_graph_EHR_Integration_with_error',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:00:00"),
+                        event_type=EventType.EHR_INTEGRATIONS.value,
+                        payload=create_integration_payload(outcome="FAILED_TO_INTEGRATE")
+                    )),
+                sourcetype="myevent")
+
+            sample_event = create_sample_event(
+                conversation_id='test_failure_point_graph_EHR_Integration_with_error',
+                registration_event_datetime=create_date_time(date=report_start, time="11:01:00"),
+                event_type=EventType.ERRORS.value,
+                payload=create_error_payload(
+                    errorCode="31",
+                    errorDescription="Random desc",
+                    failurePoint="EHR_INTEGRATION"
+                )
+            )
+
+            index.submit(
+                json.dumps(
+                    sample_event
+                ),
+                sourcetype="myevent")
+
+            # error code 09
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_multiple_failure_points',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:00:00"),
+                        event_type=EventType.EHR_RESPONSES.value
+                    )),
+                sourcetype="myevent")
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_multiple_failure_points',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:01:00"),
+                        event_type=EventType.ERRORS.value,
+                        payload=create_error_payload(
+                            errorCode="09",
+                            errorDescription="Random desc",
+                            failurePoint="Other"
+                        )
+
+                    )),
+                sourcetype="myevent")
+
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_technical_failures_snapshot_report'
+                '/gp2gp_technical_failures_snapshot_failure_point_graph_count')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$cutoff$": cutoff,
+                "$errorGraphColumn$": clicked_column,
+            })
+
+            expected_vals["0"] = expected_val
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            for row, row_values in expected_vals.items():
+                row_values_as_jq_str = ' '.join(
+                    [f"| select(.\"{key}\"==\"{value}\") " for key, value in row_values.items()]
+                )
+                self.LOG.info(f'.[{row}] {row_values_as_jq_str} ')
+                assert jq.first(
+                    f'.[{row}] {row_values_as_jq_str} ', telemetry)
+
+        finally:
+            self.delete_index(index_name)
+
     @pytest.mark.parametrize("clicked_column", ["06", "07", "09", "Integration failure", "Unknown"])
     def test_gp2gp_technical_failures_snapshot_report_failure_point_graph_percentage(self, clicked_column):
 
@@ -956,6 +1098,148 @@ class TestTechnicalFailuresSnapshotReportOutputs(TestBase):
                 "$cutoff$": cutoff,
                 "$errorGraphColumn$": clicked_column,
             })
+
+            sleep(2)
+
+            telemetry = get_telemetry_from_splunk(
+                self.savedsearch(test_query), self.splunk_service)
+            self.LOG.info(f'telemetry: {telemetry}')
+
+            # Assert
+            for row, row_values in expected_vals.items():
+                row_values_as_jq_str = ' '.join(
+                    [f"| select(.\"{key}\"==\"{value}\") " for key, value in row_values.items()]
+                )
+                self.LOG.info(f'.[{row}] {row_values_as_jq_str} ')
+                assert jq.first(
+                    f'.[{row}] {row_values_as_jq_str} ', telemetry)
+
+        finally:
+            self.delete_index(index_name)
+
+    @pytest.mark.parametrize("clicked_column, expected_val",
+                             [("06", {"failure_point": "EHR Response", "count": "100.00"}),
+                              ("09", {"failure_point": "Other", "count": "100.00"}),
+                              ("Integration failure", {"failure_point": "EHR_INTEGRATION", "count": "100.00"})])
+    def test_gp2gp_technical_failures_snapshot_report_failure_point_graph_percentage_2_failure_points_1_conv(self,
+                                                                                                        clicked_column,
+                                                                                                        expected_val):
+
+        # reporting window
+        report_start = datetime.today().date().replace(day=1)
+        report_end = datetime.today().date().replace(day=28)
+        cutoff = "0"
+
+        try:
+            # Arrange
+            index_name, index = self.create_index()
+
+            # order needs to be alphabetical for test to pass as chart orders columns in this way
+            failure_points_and_amounts = [
+                ("EHR Ready to Integrate", 3),
+                ("EHR Requested", 2),
+                ("EHR Response", 1),
+                ("Endpoint Lookup", 3),
+                ("Other", 2),
+                ("Patient General Update", 4),
+                ("Patient Trace", 1)
+            ]
+
+            expected_vals = OrderedDict()
+
+            # error code 06
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_multiple_failure_points',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:00:00"),
+                        event_type=EventType.EHR_REQUESTS.value
+                    )),
+                sourcetype="myevent")
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_multiple_failure_points',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:01:00"),
+                        event_type=EventType.ERRORS.value,
+                        payload=create_error_payload(
+                            errorCode="06",
+                            errorDescription="Random desc",
+                            failurePoint="EHR Response"
+                        )
+
+                    )),
+                sourcetype="myevent")
+
+            # FAILED_TO_INTEGRATE
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_failure_point_graph_EHR_Integration_with_error',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:00:00"),
+                        event_type=EventType.EHR_INTEGRATIONS.value,
+                        payload=create_integration_payload(outcome="FAILED_TO_INTEGRATE")
+                    )),
+                sourcetype="myevent")
+
+            sample_event = create_sample_event(
+                conversation_id='test_failure_point_graph_EHR_Integration_with_error',
+                registration_event_datetime=create_date_time(date=report_start, time="11:01:00"),
+                event_type=EventType.ERRORS.value,
+                payload=create_error_payload(
+                    errorCode="31",
+                    errorDescription="Random desc",
+                    failurePoint="EHR_INTEGRATION"
+                )
+            )
+
+            index.submit(
+                json.dumps(
+                    sample_event
+                ),
+                sourcetype="myevent")
+
+            # error code 09
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_multiple_failure_points',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:00:00"),
+                        event_type=EventType.EHR_RESPONSES.value
+                    )),
+                sourcetype="myevent")
+
+            index.submit(
+                json.dumps(
+                    create_sample_event(
+                        conversation_id='test_multiple_failure_points',
+                        registration_event_datetime=create_date_time(date=report_start, time="11:01:00"),
+                        event_type=EventType.ERRORS.value,
+                        payload=create_error_payload(
+                            errorCode="09",
+                            errorDescription="Random desc",
+                            failurePoint="Other"
+                        )
+
+                    )),
+                sourcetype="myevent")
+
+
+            # Act
+            test_query = self.generate_splunk_query_from_report(
+                'gp2gp_technical_failures_snapshot_report'
+                '/gp2gp_technical_failures_snapshot_failure_point_graph_percentage')
+
+            test_query = set_variables_on_query(test_query, {
+                "$index$": index_name,
+                "$start_time$": report_start.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$end_time$": report_end.strftime("%Y-%m-%dT%H:%m:%s"),
+                "$cutoff$": cutoff,
+                "$errorGraphColumn$": clicked_column,
+            })
+
+            expected_vals["0"] = expected_val
 
             sleep(2)
 
